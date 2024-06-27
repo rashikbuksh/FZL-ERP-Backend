714e9c9c1bfbd48763901ed4905147695c447adf
Rashik Buksh <rashikbuksh@gmail.com>
2024-06-27 14:14:18 +0600

    chore: Update schema import paths to use index.js

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 import db from "./db.js";
-import * as schema from "./schema/index.js";
+import info from "./schema/material.js";
 
-// await server
-// 	.insert(schema.users)
-// 	.values({ name: "Dan" })
-// 	.returning();
-
-const gg = await db.select().from(schema.users);
-
-console.log(gg, "users");
+// console.log(info, "users");
--- schema/hr.js
+++ schema/hr.js
@@ -5,12 +5,36 @@ import {
 	pgTable,
 	serial,
 	text,
+	uuid,
 	varchar,
 } from "drizzle-orm/pg-core";
 
 export const hr = pgSchema("hr");
 
+export const department = hr.table("department", {
+	uuid: uuid("uuid").primaryKey(),
+	department: text("department").notNull(),
+});
+
+export const designation = hr.table("designation", {
+	uuid: uuid("uuid").primaryKey(),
+	department_uuid: uuid("department_uuid").references(() => department.uuid),
+	designation: text("designation").notNull(),
+});
+
 export const users = hr.table("users", {
-	id: serial("id").primaryKey(),
-	name: text("name").unique(),
+	uuid: uuid("uuid").primaryKey(),
+	name: text("name").notNull(),
+	email: text("email").notNull(),
+	pass: text("pass").notNull(),
+	designation_uuid: uuid("designation_uuid").references(
+		() => designation.uuid
+	),
+	can_access: text("can_access").notNull(),
+	ext: text("ext"),
+	phone: text("phone"),
+	created_at: text("created_at").notNull(),
+	updated_at: text("updated_at").notNull(),
+	status: text("status").notNull(),
+	remarks: text("remarks"),
 });
