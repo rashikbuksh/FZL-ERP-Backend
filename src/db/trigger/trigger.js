7f847fce4f0a7b4b2fcab436123a1c6dd2d5c7f2
Jobayer Hossen <jobayerhossen64@gmail.com>
2024-07-31 12:59:32 +0600

    chore: Add triggers for purchase and material tables, and update trigger functions for material stock

--- src/db/trigger/trigger.js
+++ src/db/trigger/trigger.js
@@ -1,12 +1,15 @@
 import { pgTrigger } from "drizzle-orm/pg-core";
 import e from "express";
 import { pi_entry } from "../commercial/schema.js";
+import { stock, trx, used } from "../material/schema.js";
 import {
 	order_entry,
 	sfg,
 	sfg_production,
 	sfg_transaction,
 } from "../zipper/schema.js";
+
+import { entry } from "../purchase/schema.js";
 
 export const sfgProductionInsertTrigger = pgTrigger({
 	table: sfg_production,
@@ -93,5 +96,77 @@ export const orderEntryDeleteTrigger = pgTrigger({
 	when: "AFTER",
 	operation: "DELETE",
 	function: "sfg_after_zipper_order_entry_delete_trigger",
+	level: "ROW",
+});
+
+export const purchaseEntryInsertTrigger = pgTrigger({
+	table: entry,
+	when: "AFTER",
+	operation: "INSERT",
+	function: "material_stock_after_purchase_entry_insert_trigger",
+	level: "ROW",
+});
+
+export const purchaseEntryUpdateTrigger = pgTrigger({
+	table: entry,
+	when: "AFTER",
+	operation: "UPDATE",
+	function: "material_stock_after_purchase_entry_update_trigger",
+	level: "ROW",
+});
+
+export const purchaseEntryDeleteTrigger = pgTrigger({
+	table: entry,
+	when: "AFTER",
+	operation: "DELETE",
+	function: "material_stock_after_purchase_entry_delete_trigger",
+	level: "ROW",
+});
+
+export const materialUsedInsertTrigger = pgTrigger({
+	table: used,
+	when: "AFTER",
+	operation: "INSERT",
+	function: "material_stock_after_material_used_insert_trigger",
+	level: "ROW",
+});
+
+export const materialUsedUpdateTrigger = pgTrigger({
+	table: used,
+	when: "AFTER",
+	operation: "UPDATE",
+	function: "material_stock_after_material_used_update_trigger",
+	level: "ROW",
+});
+
+export const materialUsedDeleteTrigger = pgTrigger({
+	table: used,
+	when: "AFTER",
+	operation: "DELETE",
+	function: "material_stock_after_material_used_delete_trigger",
+	level: "ROW",
+});
+
+export const materialTrxInsertTrigger = pgTrigger({
+	table: trx,
+	when: "AFTER",
+	operation: "INSERT",
+	function: "material_stock_after_material_trx_insert_trigger",
+	level: "ROW",
+});
+
+export const materialTrxUpdateTrigger = pgTrigger({
+	table: trx,
+	when: "AFTER",
+	operation: "UPDATE",
+	function: "material_stock_after_material_trx_update_trigger",
+	level: "ROW",
+});
+
+export const materialTrxDeleteTrigger = pgTrigger({
+	table: trx,
+	when: "AFTER",
+	operation: "DELETE",
+	function: "material_stock_after_material_trx_delete_trigger",
 	level: "ROW",
 });
