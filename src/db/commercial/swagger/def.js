5e7509a564a40b217facad4b5e386f6dda056142
Jobayer Hossen <jobayerhossen64@gmail.com>
2024-08-25 11:18:02 +0600

    refactor: Update defCommercialBank and defCommercialLc schemas with Swagger Example Data (SED) functions

--- src/db/commercial/swagger/def.js
+++ src/db/commercial/swagger/def.js
@@ -1,50 +1,24 @@
+import SE, { SED } from '../../../util/swagger_example.js';
+
 //* ./schema.js#bank
 
-export const defCommercialBank = {
-	type: 'object',
+export const defCommercialBank = SED({
+	// type: 'object',
 	required: ['uuid', 'name', 'swift_code', 'created_at'],
 	properties: {
-		uuid: {
-			type: 'string',
-			example: 'igD0v9DIJQhJeet',
-		},
-		name: {
-			type: 'string',
-			example: 'Bangladesh Name',
-		},
-		swift_code: {
-			type: 'string',
-			example: 'BNGD',
-		},
-		address: {
-			type: 'string',
-			example: 'Dhaka, Bangladesh',
-		},
-		policy: {
-			type: 'string',
-			example: 'policy_name',
-		},
-		created_at: {
-			type: 'string',
-			format: 'date-time',
-			example: '2024-01-01 00:00:00',
-		},
-		updated_at: {
-			type: 'string',
-			format: 'date-time',
-			example: '2024-01-01 00:00:00',
-		},
-		remarks: {
-			type: 'string',
-			example: 'Remarks',
-		},
+		uuid: SE.uuid(),
+		name: SE.string('Bangladesh Bank'),
+		swift_code: SE.string('BB'),
+		address: SE.string('Dhaka, Bangladesh'),
+		policy: SE.string('Policy'),
+		created_at: SE.date_time(),
+		updated_at: SE.date_time(),
+		remarks: SE.string('remarks'),
 	},
-	xml: {
-		name: 'Commercial/Bank',
-	},
-};
+	xml: 'Commercial/Bank',
+});
 
-export const defCommercialLc = {
+export const defCommercialLc = SED({
 	type: 'object',
 	required: [
 		'uuid',
@@ -61,123 +35,33 @@ export const defCommercialLc = {
 		'created_by',
 	],
 	properties: {
-		uuid: {
-			type: 'string',
-			example: 'igD0v9DIJQhJeet',
-		},
-		party_uuid: {
-			type: 'string',
-			example: 'igD0v9DIJQhJeet',
-		},
-		file_no: {
-			type: 'string',
-			example: '1234/2024',
-		},
-		lc_number: {
-			type: 'string',
-			example: '1234/2024',
-		},
-		lc_date: {
-			type: 'string',
-			example: '2024-01-01 00:00:00',
-		},
-		payment_value: {
-			type: 'number',
-			example: 1000.0,
-		},
-		payment_date: {
-			type: 'string',
-			format: 'date-time',
-			example: '2024-01-01 00:00:00',
-		},
-		ldbc_fdbc: {
-			type: 'string',
-			example: 'LDBC',
-		},
-		acceptance_date: {
-			type: 'string',
-			format: 'date-time',
-			example: '2024-01-01 00:00:00',
-		},
-		maturity_date: {
-			type: 'string',
-			format: 'date-time',
-			example: '2024-01-01 00:00:00',
-		},
-		commercial_executive: {
-			type: 'string',
-			example: 'John Doe',
-		},
-		party_bank: {
-			type: 'string',
-			example: 'Bangladesh Bank',
-		},
-		production_complete: {
-			type: 'integer',
-			example: 0,
-		},
-		lc_cancel: {
-			type: 'integer',
-			example: 0,
-		},
-		handover_date: {
-			type: 'string',
-			format: 'date-time',
-			example: '2024-01-01 00:00:00',
-		},
-		shipment_date: {
-			type: 'string',
-			format: 'date-time',
-			example: '2024-01-01 00:00:00',
-		},
-		expiry_date: {
-			type: 'string',
-			format: 'date-time',
-			example: '2024-01-01 00:00:00',
-		},
-		ud_no: {
-			type: 'string',
-			example: '1234/2024',
-		},
-		ud_received: {
-			type: 'string',
-			example: '2024-01-01 00:00:00',
-		},
-		at_sight: {
-			type: 'string',
-			example: 'At Sight',
-		},
-		amd_date: {
-			type: 'string',
-			format: 'date-time',
-			example: '2024-01-01 00:00:00',
-		},
-		amd_count: {
-			type: 'integer',
-			example: 0,
-		},
-		problematical: {
-			type: 'integer',
-			example: 0,
-		},
-		epz: {
-			type: 'integer',
-			example: 0,
-		},
-		created_by: {
-			type: 'string',
-			example: 'igD0v9DIJQhJeet',
-		},
-		created_at: {
-			type: 'string',
-			format: 'date-time',
-			example: '2024-01-01 00:00:00',
-		},
-		updated_at: {
-			type: 'string',
-			format: 'date-time',
-			example: '2024-01-01 00:00:00',
-		},
+		uuid: SE.uuid(),
+		party_uuid: SE.uuid(),
+		file_no: SE.string('1234/2024'),
+		lc_number: SE.string('1234/2024'),
+		lc_date: SE.date_time(),
+		payment_value: SE.number(1000.0),
+		payment_date: SE.date_time(),
+		ldbc_fdbc: SE.string('LDBC'),
+		acceptance_date: SE.date_time(),
+		maturity_date: SE.date_time(),
+		commercial_executive: SE.string('John Doe'),
+		party_bank: SE.string('Bangladesh Bank'),
+		production_complete: SE.integer(0),
+		lc_cancel: SE.integer(0),
+		handover_date: SE.date_time(),
+		shipment_date: SE.date_time(),
+		expiry_date: SE.date_time(),
+		ud_no: SE.string('1234/2024'),
+		ud_received: SE.date_time(),
+		at_sight: SE.string('At Sight'),
+		amd_date: SE.date_time(),
+		amd_count: SE.integer(0),
+		problematical: SE.integer(0),
+		epz: SE.integer(0),
+		created_by: SE.uuid(),
+		created_at: SE.date_time(),
+		updated_at: SE.date_time(),
 		remarks: {
 			type: 'string',
 			example: 'Remarks',
@@ -186,7 +70,7 @@ export const defCommercialLc = {
 	xml: {
 		name: 'Commercial/Lc',
 	},
-};
+});
 
 export const defCommercialPi = {
 	type: 'object',
