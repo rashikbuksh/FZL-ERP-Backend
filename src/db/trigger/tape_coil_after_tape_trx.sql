7e20c46dd59dfdfb76ee64b865d9715c561493f1
Rashik Buksh <rashikbuksh@gmail.com>
2024-09-19 17:39:57 +0600

    Refactor tape_coil_after_tape_trx.sql trigger to fix typo in variable name

--- src/db/trigger/tape_coil_after_tape_trx.sql
+++ src/db/trigger/tape_coil_after_tape_trx.sql
@@ -38,7 +38,7 @@ BEGIN
     --Update zipper.tape_coil table
     UPDATE zipper.tape_coil 
     SET
-       trx_quantity_in_dying = trx_quantity_in_dying + CASE WHEN NEW.to_section = 'dyeing' THEN NEW.trx_quantity ELSE 0 END - CASE WHEN OLD.to_section = 'dying' THEN OLD.trx_quantity ELSE 0 END,
+       trx_quantity_in_dying = trx_quantity_in_dying + CASE WHEN NEW.to_section = 'dyeing' THEN NEW.trx_quantity ELSE 0 END - CASE WHEN OLD.to_section = 'dyeing' THEN OLD.trx_quantity ELSE 0 END,
        trx_quantity_in_coil = trx_quantity_in_coil + CASE WHEN NEW.to_section = 'coil' THEN NEW.trx_quantity ELSE 0 END - CASE WHEN OLD.to_section = 'coil' THEN OLD.trx_quantity ELSE 0 END,
        quantity = quantity - NEW.trx_quantity + OLD.trx_quantity,
          trx_quantity_in_dying = trx_quantity_in_dying + CASE WHEN NEW.to_section = 'dyeing' AND (SELECT name FROM public.properties where zipper.tape_coil.item_uuid = public.properties.uuid) = 'nylon' THEN NEW.trx_quantity ELSE 0 END - CASE WHEN OLD.to_section = 'dyeing' AND (SELECT name FROM public.properties where zipper.tape_coil.item_uuid = public.properties.uuid) = 'nylon' THEN OLD.trx_quantity ELSE 0 END
