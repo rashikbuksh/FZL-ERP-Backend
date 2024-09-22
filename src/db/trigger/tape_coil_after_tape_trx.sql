bb1ec6a8dcd443cf2829da9c8cee59ddf0260be5
Rashik Buksh <rashikbuksh@gmail.com>
2024-09-22 11:35:48 +0600

    Refactor tape_coil_after_tape_trx.sql trigger function

--- src/db/trigger/tape_coil_after_tape_trx.sql
+++ src/db/trigger/tape_coil_after_tape_trx.sql
@@ -7,15 +7,17 @@ BEGIN
         -- Tape Trx to Coil Or Dyeing
         quantity = quantity - CASE WHEN NEW.to_section = 'dyeing' OR NEW.to_section = 'coil' THEN NEW.trx_quantity ELSE 0 END,
         -- Coil To Dyeing
-        quantity_in_coil = quantity_in_coil - CASE WHEN NEW.to_section = 'coil_dyeing' AND (SELECT lower(name) FROM public.properties where zipper.tape_coil.item_uuid = public.properties.uuid) = 'nylon' THEN NEW.trx_quantity ELSE 0 END,
+        quantity_in_coil = quantity_in_coil 
+        - CASE WHEN NEW.to_section = 'coil_dyeing' AND (SELECT lower(name) FROM public.properties where zipper.tape_coil.item_uuid = public.properties.uuid) = 'nylon' THEN NEW.trx_quantity ELSE 0 END,
         -- Tape AND Coil Dyeing Trx
-        trx_quantity_in_dying = trx_quantity_in_dying + CASE WHEN NEW.to_section = 'dyeing' OR NEW.to_section = 'coil_dyeing' THEN NEW.trx_quantity ELSE 0 END,
+        trx_quantity_in_dying = trx_quantity_in_dying 
+        + CASE WHEN NEW.to_section = 'dyeing' OR NEW.to_section = 'coil_dyeing' THEN NEW.trx_quantity ELSE 0 END 
+        - CASE WHEN NEW.to_section = 'stock' THEN NEW.trx_quantity ELSE 0 END,
         
         -- Tape to Coil Trx 
         trx_quantity_in_coil = trx_quantity_in_coil + CASE WHEN NEW.to_section = 'coil' THEN NEW.trx_quantity ELSE 0 END,
 
         -- Dyed Tape or Coil Stock
-        trx_quantity_in_dying = trx_quantity_in_dying - CASE WHEN NEW.to_section = 'stock' THEN NEW.trx_quantity ELSE 0 END,
         stock_quantity = stock_quantity + CASE WHEN NEW.to_section = 'stock' THEN NEW.trx_quantity ELSE 0 END
 
     WHERE uuid = NEW.tape_coil_uuid;
@@ -33,11 +35,12 @@ BEGIN
         -- Coil To Dyeing
         quantity_in_coil = quantity_in_coil + CASE WHEN OLD.to_section = 'coil_dyeing' AND (SELECT lower(name) FROM public.properties WHERE zipper.tape_coil.item_uuid = public.properties.uuid) = 'nylon' THEN OLD.trx_quantity ELSE 0 END,
         -- Tape AND Coil Dyeing Trx
-        trx_quantity_in_dying = trx_quantity_in_dying - CASE WHEN OLD.to_section = 'dyeing' OR OLD.to_section = 'coil_dyeing' THEN OLD.trx_quantity ELSE 0 END,
+        trx_quantity_in_dying = trx_quantity_in_dying 
+        - CASE WHEN OLD.to_section = 'dyeing' OR OLD.to_section = 'coil_dyeing' THEN OLD.trx_quantity ELSE 0 END
+        + CASE WHEN OLD.to_section = 'stock' THEN OLD.trx_quantity ELSE 0 END,
         -- Tape to Coil Trx 
         trx_quantity_in_coil = trx_quantity_in_coil - CASE WHEN OLD.to_section = 'coil' THEN OLD.trx_quantity ELSE 0 END,
         -- Dyed Tape or Coil Stock
-        trx_quantity_in_dying = trx_quantity_in_dying + CASE WHEN OLD.to_section = 'stock' THEN OLD.trx_quantity ELSE 0 END,
         stock_quantity = stock_quantity - CASE WHEN OLD.to_section = 'stock' THEN OLD.trx_quantity ELSE 0 END
     WHERE uuid = OLD.tape_coil_uuid;
     RETURN OLD;
@@ -72,6 +75,13 @@ BEGIN
         END - CASE 
             WHEN OLD.to_section = 'dyeing' OR OLD.to_section = 'coil_dyeing' THEN OLD.trx_quantity 
             ELSE 0 
+        END
+        - CASE 
+            WHEN NEW.to_section = 'stock' THEN NEW.trx_quantity 
+            ELSE 0 
+        END + CASE 
+            WHEN OLD.to_section = 'stock' THEN OLD.trx_quantity 
+            ELSE 0 
         END,
         -- Tape to Coil Trx 
         trx_quantity_in_coil = trx_quantity_in_coil + CASE 
@@ -82,13 +92,6 @@ BEGIN
             ELSE 0 
         END,
         -- Dyed Tape or Coil Stock
-        trx_quantity_in_dying = trx_quantity_in_dying - CASE 
-            WHEN NEW.to_section = 'stock' THEN NEW.trx_quantity 
-            ELSE 0 
-        END + CASE 
-            WHEN OLD.to_section = 'stock' THEN OLD.trx_quantity 
-            ELSE 0 
-        END,
         stock_quantity = stock_quantity + CASE 
             WHEN NEW.to_section = 'stock' THEN NEW.trx_quantity 
             ELSE 0 
