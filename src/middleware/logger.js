62fdef14bcec5ab233daf3d1140de93fc6ee7cb0
Md. Shahadat Anik Sheikh <git.shahadat.anik@gmail.com>
2024-09-23 17:31:38 +0600

    perf: :wheelchair: Morger And Logger
    added some login funtionalty to get the log

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,29 +1,36 @@
-import morgan from 'morgan';
 import winston from 'winston';
+import morgan from 'morgan';
+require('winston-daily-rotate-file');
 
 const { combine, timestamp, json, colorize, printf } = winston.format;
+
+const fileRotateTransport = new winston.transports.DailyRotateFile({
+	filename: 'combined-%DATE%.log',
+	datePattern: 'YYYY-MM-DD',
+	maxFiles: '14d',
+});
 
 const logger = winston.createLogger({
 	format: combine(
 		colorize({ all: true }),
+		errors({ stack: true }),
 		timestamp({
 			format: 'YYYY-MM-DD hh:mm:ss A',
 		}),
-		json(),
-		printf(({ timestamp, level, message, ...data }) => {
-			const response = {
-				level,
-				message,
-				data, // metadata
-			};
-
-			return JSON.stringify(response);
-		})
+		json()
 	),
-	transports: [
-		new winston.transports.Console(),
-		new winston.transports.File({ filename: 'error.log', level: 'error' }),
-		new winston.transports.File({ filename: 'combined.log' }),
+	// transports: [
+	// 	new winston.transports.Console(),
+	// 	new winston.transports.File({ filename: 'error.log', level: 'error' }),
+	// 	new winston.transports.File({ filename: 'combined.log' }),
+	// ],
+
+	transports: [fileRotateTransport],
+	exceptionHandlers: [
+		new winston.transports.File({ filename: 'exception.log' }),
+	],
+	rejectionHandlers: [
+		new winston.transports.File({ filename: 'rejections.log' }),
 	],
 });
 
@@ -39,7 +46,7 @@ export const morganMiddleware = morgan(
 	},
 	{
 		stream: {
-			// * Configure Morgan to use our custom logger with the http severity
+			// Configure Morgan to use our custom logger with the http severity
 			write: (message) => {
 				const data = JSON.parse(message);
 				logger.http(`incoming-request`, data);
@@ -47,5 +54,4 @@ export const morganMiddleware = morgan(
 		},
 	}
 );
-
 export default logger;
