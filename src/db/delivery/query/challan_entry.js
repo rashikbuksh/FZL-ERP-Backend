bd103dabfd5f5a43bac22bb62ca7cd2b29148faf
rashikbuksh <rashikbuksh@gmail.com>
2024-09-24 21:03:47 +0600

    Refactor query to include selectPackingListForChallan and selectPackingListForChallanMulti functions

--- src/db/delivery/query/challan_entry.js
+++ src/db/delivery/query/challan_entry.js
@@ -234,3 +234,116 @@ export async function selectChallanEntryByChallanUuid(req, res, next) {
 		handleError({ error, res });
 	}
 }
+
+export async function selectPackingListForChallan(req, res, next) {
+	if (!(await validateRequest(req, next))) return;
+
+	const query = sql`
+		SELECT 
+			challan_entry.uuid as uuid,
+			challan_entry.challan_uuid,
+			challan.assign_to AS challan_assign_to,
+			assign_to_user.name AS challan_assign_to_name,
+			challan.created_by AS challan_created_by,
+			created_by_user.name AS challan_created_by_name,
+			challan_entry.packing_list_uuid,
+			CONCAT('PL', TO_CHAR(pl.created_at, 'YY'), '-', LPAD(pl.id::text, 4, '0')) AS packing_number,
+			challan_entry.created_at,
+			challan_entry.updated_at,
+			ple.uuid as packing_list_entry_uuid,
+			ple.sfg_uuid,
+			ple.quantity,
+			ple.short_quantity,
+			ple.reject_quantity,
+			ple.remarks as remarks,
+			vodf.order_info_uuid as order_info_uuid,
+			vodf.order_number,
+			vodf.item_description,
+			vodf.order_description_uuid,
+			concat(oe.style, ' / ', oe.color, ' / ', oe.size) as style_color_size,
+			oe.quantity as order_quantity,
+			sfg.warehouse as warehouse,
+			sfg.delivered as delivered,
+			(oe.quantity - sfg.warehouse) as balance_quantity
+		FROM 
+			delivery.challan_entry
+		LEFT JOIN 
+			delivery.challan ON challan_entry.challan_uuid = challan.uuid
+		LEFT JOIN 
+			hr.users assign_to_user ON challan.assign_to = assign_to_user.uuid
+		LEFT JOIN 
+			hr.users created_by_user ON challan.created_by = created_by_user.uuid
+		LEFT JOIN 
+			delivery.packing_list pl ON challan_entry.packing_list_uuid = pl.uuid
+		LEFT JOIN 
+			delivery.packing_list_entry ple ON pl.uuid = ple.packing_list_uuid
+		LEFT JOIN 
+			zipper.sfg sfg ON ple.sfg_uuid = sfg.uuid
+		LEFT JOIN
+			zipper.order_entry oe ON sfg.order_entry_uuid = oe.uuid
+		LEFT JOIN 
+			zipper.v_order_details_full vodf ON oe.order_description_uuid = vodf.order_description_uuid
+		WHERE 
+			ple.packing_list_uuid = ${req.params.packing_list_uuid} AND challan_entry.uuid IS NULL;
+	`;
+
+	const challan_entryPromise = db.execute(query);
+
+	try {
+		const data = await challan_entryPromise;
+		const toast = {
+			status: 200,
+			type: 'select',
+			message: 'Challan_entry',
+		};
+
+		return res.status(200).json({ toast, data: data.rows });
+	} catch (error) {
+		handleError({ error, res });
+	}
+}
+
+export async function selectPackingListForChallanMulti(req, res, next) {
+	if (!(await validateRequest(req, next))) return;
+	try {
+		const { packing_list_uuids } = req.params;
+
+		const api = await createApi(req);
+
+		const fetchData = async (packing_list_uuid) =>
+			await api
+				.get(
+					`/delivery//challan-entry-for-packing-list/by/${packing_list_uuid}`
+				)
+				.then((response) => response);
+
+		const packing_list_uuid = packing_list_uuids
+			.split(',')
+			.map(String)
+			.map((String) => [String])
+			.flat();
+
+		const packing_list_entryPromise = await Promise.all(
+			packing_list_uuid.map((uuid) => fetchData(uuid))
+		);
+
+		const response = {
+			packing_list_entry: packing_list_entryPromise?.reduce(
+				(acc, result) => {
+					return [...acc, ...result?.data?.data];
+				},
+				[]
+			),
+		};
+
+		const toast = {
+			status: 200,
+			type: 'select',
+			message: 'Packing_list_entry',
+		};
+
+		return await res.status(200).json({ toast, data: response });
+	} catch (error) {
+		await handleError({ error, res });
+	}
+}
