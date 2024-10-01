2c6348d384cdbcd6ae2c3273b87141a48faff199
rashikbuksh <rashikbuksh@gmail.com>
2024-10-01 16:21:30 +0600

    Refactor selectByOrderInfoUuid function to exclude already assigned challan entries

--- src/db/Thread/query/challan.js
+++ src/db/Thread/query/challan.js
@@ -198,7 +198,8 @@ export async function selectByOrderInfoUuid(req, res, next) {
 		FROM thread.order_entry toe
 		LEFT JOIN thread.order_info toi ON toe.order_info_uuid = toi.uuid
 		LEFT JOIN thread.count_length cl ON toe.count_length_uuid = cl.uuid
-		WHERE toe.order_info_uuid = ${req.params.order_info_uuid} AND (toe.quantity - toe.delivered) > 0
+		LEFT JOIN thread.challan_entry ON toe.uuid = challan_entry.order_entry_uuid
+		WHERE toe.order_info_uuid = ${req.params.order_info_uuid} AND (toe.quantity - toe.delivered) > 0 AND challan_entry.uuid IS NULL
 	`;
 
 	const resultPromise = db.execute(query);
@@ -234,16 +235,48 @@ export async function selectThreadChallanDetailsByChallanUuid(req, res, next) {
 			fetchData('/thread/challan'),
 			fetchData('/thread/challan-entry/by'),
 		]);
-		// if (is_update === 'true') {
-		// 	let query = sql`
 
-		// 	`;
-		// }
+		let query_data;
+
+		if (is_update === 'true') {
+			const order_info_uuid = challan?.data?.data?.order_info_uuid;
+			let query = sql`
+				SELECT
+					concat('TO', to_char(toi.created_at, 'YY'), '-', LPAD(toi.id::text, 4, '0')) AS order_number,
+					toe.order_info_uuid as order_info_uuid,
+					toe.uuid as order_entry_uuid,
+					toe.count_length_uuid,
+					cl.count as count,
+					cl.length as length,
+					cl.cone_per_carton,
+					toe.style as style,
+					toe.color as color,
+					toe.quantity as order_quantity,
+					toe.delivered as delivered,
+					toe.warehouse as warehouse,
+					toe.bleaching as bleaching,
+					(toe.quantity - toe.delivered) as balance_quantity,
+					false as is_checked
+				FROM thread.order_entry toe
+				LEFT JOIN thread.order_info toi ON toe.order_info_uuid = toi.uuid
+				LEFT JOIN thread.count_length cl ON toe.count_length_uuid = cl.uuid
+				LEFT JOIN thread.challan_entry ON toe.uuid = challan_entry.order_entry_uuid
+				WHERE toe.order_info_uuid = ${order_info_uuid} AND (toe.quantity - toe.delivered) > 0 AND challan_entry.uuid IS NULL
+			`;
+			query_data = await db.execute(query);
+		}
 
 		const response = {
 			...challan?.data?.data,
 			challan_entry: challan_entry?.data?.data || [],
 		};
+
+		if (is_update == 'true') {
+			response.challan_entry = [
+				...response.challan_entry,
+				...query_data?.rows,
+			];
+		}
 
 		const toast = {
 			status: 200,
