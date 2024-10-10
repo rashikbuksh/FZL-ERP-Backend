ec19d650f7f9ac8a9b54a9ef2d7a186e2beddac4
Rashik Buksh <rashikbuksh@gmail.com>
2024-10-10 15:12:42 +0600

    Refactor trigger functions for SFG after challan receive status

--- src/db/trigger/sfg_after_challan_receive_status.sql
+++ src/db/trigger/sfg_after_challan_receive_status.sql
@@ -42,11 +42,11 @@ FOR EACH ROW
 EXECUTE FUNCTION delivery.sfg_after_challan_receive_status_insert_function();
 
 CREATE OR REPLACE TRIGGER sfg_after_challan_receive_status_delete
-AFTER DELETE ON delivery.packing_list_entry
+AFTER DELETE ON delivery.challan
 FOR EACH ROW
 EXECUTE FUNCTION delivery.sfg_after_challan_receive_status_delete_function();
 
 CREATE OR REPLACE TRIGGER sfg_after_challan_receive_status_update
-AFTER UPDATE ON delivery.packing_list_entry
+AFTER UPDATE ON delivery.challan
 FOR EACH ROW
 EXECUTE FUNCTION delivery.sfg_after_challan_receive_status_update_function();
