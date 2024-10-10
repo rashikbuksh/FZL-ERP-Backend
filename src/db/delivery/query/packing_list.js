e07299a8d0865acab56a49463cc24594edfa3e47
Rashik Buksh <rashikbuksh@gmail.com>
2024-10-10 14:48:10 +0600

    Refactor query file to fix syntax error in size calculation

--- src/db/delivery/query/packing_list.js
+++ src/db/delivery/query/packing_list.js
@@ -267,7 +267,7 @@ export async function selectAllOrderForPackingList(req, res, next) {
 			CASE 
                 WHEN vodf.is_inch = 1 THEN CAST(CAST(oe.size AS NUMERIC) * 2.54 AS TEXT)
                 ELSE oe.size
-                ND as size,
+                END as size,
 			vodf.is_inch,
 			concat(oe.style, ' / ', oe.color, ' / ', oe.size) as style_color_size,
 			oe.quantity::float8  as order_quantity,
