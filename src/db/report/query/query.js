2b3d317ba1803eed8a0e175db8cc12902f2aed22
Rashik Buksh <rashikbuksh@gmail.com>
2024-10-15 14:53:59 +0600

    Refactor report route to include production report director endpoint

--- src/db/report/query/query.js
+++ src/db/report/query/query.js
@@ -763,14 +763,223 @@ export async function ProductionReportDirector(req, res, next) {
 	try {
 		const data = await resultPromise;
 
+		// row group using item_name, then party_name, then order_number, then item_description
+		const groupedData = data?.rows.reduce((acc, row) => {
+			const itemIndex = acc.findIndex(
+				(item) => item.item_name === row.item_name
+			);
+			if (itemIndex === -1) {
+				acc.push({
+					item_name: row.item_name,
+					parties: [
+						{
+							party_name: row.party_name,
+							orders: [
+								{
+									order_number: row.order_number,
+									items: [
+										{
+											item_description:
+												row.item_description,
+											total_close_end_quantity:
+												row.total_close_end_quantity,
+											total_open_end_quantity:
+												row.total_open_end_quantity,
+											total_quantity: row.total_quantity,
+										},
+									],
+								},
+							],
+						},
+					],
+				});
+			} else {
+				const partyIndex = acc[itemIndex].parties.findIndex(
+					(party) => party.party_name === row.party_name
+				);
+				if (partyIndex === -1) {
+					acc[itemIndex].parties.push({
+						party_name: row.party_name,
+						orders: [
+							{
+								order_number: row.order_number,
+								items: [
+									{
+										item_description: row.item_description,
+										total_close_end_quantity:
+											row.total_close_end_quantity,
+										total_open_end_quantity:
+											row.total_open_end_quantity,
+										total_quantity: row.total_quantity,
+									},
+								],
+							},
+						],
+					});
+				} else {
+					const orderIndex = acc[itemIndex].parties[
+						partyIndex
+					].orders.findIndex(
+						(order) => order.order_number === row.order_number
+					);
+					if (orderIndex === -1) {
+						acc[itemIndex].parties[partyIndex].orders.push({
+							order_number: row.order_number,
+							items: [
+								{
+									item_description: row.item_description,
+									total_close_end_quantity:
+										row.total_close_end_quantity,
+									total_open_end_quantity:
+										row.total_open_end_quantity,
+									total_quantity: row.total_quantity,
+								},
+							],
+						});
+					} else {
+						acc[itemIndex].parties[partyIndex].orders[
+							orderIndex
+						].items.push({
+							item_description: row.item_description,
+							total_close_end_quantity:
+								row.total_close_end_quantity,
+							total_open_end_quantity:
+								row.total_open_end_quantity,
+							total_quantity: row.total_quantity,
+						});
+					}
+				}
+			}
+			return acc;
+		}, []);
+
 		const toast = {
 			status: 200,
 			type: 'select_all',
 			message: 'Production Report Director',
 		};
 
+		res.status(200).json({ toast, data: groupedData });
+	} catch (error) {
+		await handleError({ error, res });
+	}
+}
+
+export async function ProductionReportThreadDirector(req, res, next) {
+	const query = sql`
+            SELECT 
+                order_info.uuid,
+                'Sewing Thread' as item_name,
+                order_info.party_uuid,
+                party.name as party_name,
+                CONCAT('TO', to_char(order_info.created_at, 'YY'), '-', LPAD(order_info.id::text, 4, '0')) as order_number,
+                count_length.count,
+                count_length.length,
+                CONCAT(count_length.count, ' - ', count_length.length) as count_length_name,
+                coalesce(prod_quantity.total_quantity,0) as total_quantity,
+            FROM
+                thread.order_info
+            LEFT JOIN
+                thread.order_entry ON order_entry.order_info_uuid = order_info.uuid
+            LEFT JOIN
+                thread.count_length ON order_entry.count_length_uuid = count_length.uuid
+            LEFT JOIN
+                public.party ON order_info.party_uuid = party.uuid
+            LEFT JOIN
+                public.marketing ON order_info.marketing_uuid = marketing.uuid
+            LEFT JOIN (
+                SELECT
+                    SUM(production_quantity) as total_quantity,
+                    order_info_uuid
+                FROM
+                    thread.batch_entry
+                GROUP BY
+                    order_info_uuid
+            ) prod_quantity ON order_info.uuid = prod_quantity.order_info_uuid
+            WHERE order_info.uuid IS NOT NULL
+            ORDER BY order_info.created_at DESC
+    `;
+
+	const resultPromise = db.execute(query);
+
+	try {
+		const data = await resultPromise;
+
+		const toast = {
+			status: 200,
+			type: 'select_all',
+			message: 'Production Report Director Thread',
+		};
+
 		res.status(200).json({ toast, data: data?.rows });
 	} catch (error) {
 		await handleError({ error, res });
 	}
-} // NOTE: Incomplete
+} // NOTE: incomplete
+
+export async function ProductionReportSnm(req, res, next) {
+	const query = sql`
+            SELECT 
+                vodf.order_info_uuid,
+                vodf.item,
+                vodf.item_name,
+                vodf.order_number,
+                vodf.party_uuid,
+                vodf.party_name,
+                vodf.order_description_uuid,
+                vodf.item_description,
+                vodf.end_type,
+                vodf.end_type_name,
+                oe,uuid as order_entry_uuid,
+                oe.size,
+                coalesce(close_end_sum.total_close_end_quantity,0) as total_close_end_quantity,
+                coalesce(open_end_sum.total_open_end_quantity,0) as total_open_end_quantity,
+                coalesce(close_end_sum.total_close_end_quantity + open_end_sum.total_open_end_quantity,0) as total_quantity
+            FROM
+                zipper.v_order_details_full vodf
+            LEFT JOIN 
+                zipper.order_entry oe ON vodf.order_description_uuid = oe.order_description_uuid
+            LEFT JOIN (
+                SELECT 
+                    coalesce(SUM(CASE WHEN lower(vodf.end_type_name) = 'close end' THEN sfg_production.production_quantity::float8 ELSE 0 END), 0)::float8 AS total_close_end_quantity,
+                    oe.uuid as order_entry_uuid
+                FROM
+                    zipper.sfg_production
+                    LEFT JOIN zipper.sfg ON sfg_production.sfg_uuid = sfg.uuid
+                    LEFT JOIN zipper.order_entry oe ON sfg.order_entry_uuid = oe.uuid
+                    LEFT JOIN zipper.v_order_details_full vodf ON oe.order_description_uuid = vodf.order_description_uuid
+                GROUP BY
+                    oe.uuid
+            ) close_end_sum ON oe.uuid = close_end_sum.order_entry_uuid
+            LEFT JOIN (
+                SELECT 
+                    coalesce(SUM(CASE WHEN lower(vodf.end_type_name) = 'open end' THEN sfg_production.production_quantity::float8 ELSE 0 END), 0)::float8 AS total_open_end_quantity,
+                    oe.uuid as order_entry_uuid
+                FROM
+                    zipper.sfg_production
+                    LEFT JOIN zipper.sfg ON sfg_production.sfg_uuid = sfg.uuid
+                    LEFT JOIN zipper.order_entry oe ON sfg.order_entry_uuid = oe.uuid
+                    LEFT JOIN zipper.v_order_details_full vodf ON oe.order_description_uuid = vodf.order_description_uuid
+                GROUP BY
+                    oe.uuid
+            ) open_end_sum ON oe.uuid = open_end_sum.order_entry_uuid
+            WHERE vodf.order_description_uuid IS NOT NULL
+            ORDER BY vodf.item_name DESC
+    `;
+
+	const resultPromise = db.execute(query);
+
+	try {
+		const data = await resultPromise;
+
+		const toast = {
+			status: 200,
+			type: 'select_all',
+			message: 'Production Report S&M',
+		};
+
+		res.status(200).json({ toast, data: data?.rows });
+	} catch (error) {
+		await handleError({ error, res });
+	}
+}
