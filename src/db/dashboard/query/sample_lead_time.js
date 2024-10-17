df719c435d3a048eb508a208ec897ffef095182f
rashikbuksh <rashikbuksh@gmail.com>
2024-10-17 18:31:27 +0600

    Refactor selectSampleLeadTime query to update column names and count the number of orders

--- src/db/dashboard/query/sample_lead_time.js
+++ src/db/dashboard/query/sample_lead_time.js
@@ -6,7 +6,7 @@ export async function selectSampleLeadTime(req, res, next) {
 	if (!(await validateRequest(req, next))) return;
 
 	const query = sql`
-    SELECT * FROM (
+    WITH combined_results AS (
         SELECT DISTINCT
             CONCAT('Z', to_char(oi.created_at, 'YY'), '-', LPAD(oi.id::text, 4, '0')) as sample_order_no,
             oi.created_at as issue_date,
@@ -24,7 +24,7 @@ export async function selectSampleLeadTime(req, res, next) {
             LEFT JOIN zipper.order_description od ON oi.uuid = od.order_info_uuid
             LEFT JOIN zipper.order_entry oe ON od.uuid = oe.order_description_uuid
         WHERE
-        oi.is_sample = 1 
+            oi.is_sample = 1 
 
         UNION
         SELECT DISTINCT
@@ -43,21 +43,37 @@ export async function selectSampleLeadTime(req, res, next) {
             LEFT JOIN thread.challan tc ON tce.challan_uuid = tc.uuid
         WHERE 
             toi.is_sample = 1
-            ) AS combined_results
-                LIMIT 10;
+        )
+    SELECT *, (SELECT COUNT(*) FROM combined_results) as total_number
+    FROM combined_results
+    ORDER BY issue_date DESC
     `;
 
 	const resultPromise = db.execute(query);
 
 	try {
 		const data = await resultPromise;
+
+		const totalNumberOfOrder =
+			data.rows.length > 0 ? data.rows[0].total_number : 0;
+
+		const chartData = data.rows.map((row) => {
+			const { total_number, ...rest } = row;
+			return rest;
+		});
+
+		const response = {
+			total_number: totalNumberOfOrder,
+			chart_data: chartData,
+		};
+
 		const toast = {
 			status: 200,
 			type: 'select',
 			message: 'Sample Lead Time',
 		};
 
-		return res.status(200).json({ toast, data: data.rows });
+		return res.status(200).json({ toast, data: response });
 	} catch (error) {
 		handleError({ error, res });
 	}
