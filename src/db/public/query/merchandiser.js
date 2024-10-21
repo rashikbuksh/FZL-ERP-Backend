1d8160e73f6777a3ba5c629c641df7d1275d5707
Rashik Buksh <rashikbuksh@gmail.com>
2024-10-21 13:49:49 +0600

    Refactor pagination logic in selectAll function to use consistent naming conventions for next and previous page variables

--- src/db/public/query/merchandiser.js
+++ src/db/public/query/merchandiser.js
@@ -115,12 +115,12 @@ export async function selectAll(req, res, next) {
 			total_page: Math.ceil(
 				resultPromiseForCount.length / req.query.limit
 			),
-			nextPage:
+			next_page:
 				Number(req.query.page) + 1 >
 				Math.ceil(resultPromiseForCount.length / req.query.limit)
 					? null
 					: Number(req.query.page) + 1,
-			prevPage: req.query.page - 1 <= 0 ? null : req.query.page - 1,
+			prev_page: req.query.page - 1 <= 0 ? null : req.query.page - 1,
 		};
 
 		const data = await baseQuery;
