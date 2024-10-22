703d92cf508ce1840b5fbd464dcbb45f64682095
Rashik Buksh <rashikbuksh@gmail.com>
2024-10-22 18:11:44 +0600

    Refactor multi_color_dashboard.js to include new selectAll query with additional fields

--- src/db/zipper/query/multi_color_dashboard.js
+++ src/db/zipper/query/multi_color_dashboard.js
@@ -103,39 +103,7 @@ export async function selectAll(req, res, next) {
 		LEFT JOIN
 			zipper.v_order_details_full vodf ON mcd.order_description_uuid = vodf.order_description_uuid;
 	`;
-	const resultPromise = db
-		.select({
-			uuid: multi_color_dashboard.uuid,
-			order_description_uuid:
-				multi_color_dashboard.order_description_uuid,
-			expected_tape_quantity: decimalToNumber(
-				multi_color_dashboard.expected_tape_quantity
-			),
-			is_swatch_approved: multi_color_dashboard.is_swatch_approved,
-			tape_quantity: multi_color_dashboard.tape_quantity,
-			coil_uuid: multi_color_dashboard.coil_uuid,
-			coil_name: materialSchema.info.name,
-			coil_quantity: decimalToNumber(multi_color_dashboard.coil_quantity),
-			thread_name: multi_color_dashboard.thread_name,
-			thread_quantity: multi_color_dashboard.thread_quantity,
-			is_coil_received_sewing:
-				multi_color_dashboard.is_coil_received_sewing,
-			is_thread_received_sewing:
-				multi_color_dashboard.is_thread_received_sewing,
-			remarks: multi_color_dashboard.remarks,
-		})
-		.from(multi_color_dashboard)
-		.leftJoin(
-			materialSchema.info,
-			eq(multi_color_dashboard.coil_uuid, materialSchema.info.uuid)
-		)
-		.leftJoin(
-			sql`zipper.v_order_details`,
-			eq(
-				multi_color_dashboard.order_description_uuid,
-				sql`v_order_details.order_description_uuid`
-			)
-		);
+	const resultPromise = db.execute(query);
 
 	try {
 		const data = await resultPromise;
@@ -144,50 +112,41 @@ export async function selectAll(req, res, next) {
 			type: 'select',
 			message: 'multi_color_dashboard',
 		};
-		res.status(200).json({ toast, data });
+		res.status(200).json({ toast, data: data?.rows });
 	} catch (error) {
 		await handleError({ error, res });
 	}
 }
 
 export async function select(req, res, next) {
-	const resultPromise = db
-		.select({
-			uuid: multi_color_dashboard.uuid,
-			order_description_uuid:
-				multi_color_dashboard.order_description_uuid,
-			expected_tape_quantity: decimalToNumber(
-				multi_color_dashboard.expected_tape_quantity
-			),
-			production_quantity_in_kg: decimalToNumber(
-				multi_color_dashboard.production_quantity_in_kg
-			),
-			is_swatch_approved: multi_color_dashboard.is_swatch_approved,
-			tape_quantity: multi_color_dashboard.tape_quantity,
-			coil_uuid: multi_color_dashboard.coil_uuid,
-			coil_name: materialSchema.info.name,
-			coil_quantity: decimalToNumber(multi_color_dashboard.coil_quantity),
-			thread_name: multi_color_dashboard.thread_name,
-			thread_quantity: multi_color_dashboard.thread_quantity,
-			is_coil_received_sewing:
-				multi_color_dashboard.is_coil_received_sewing,
-			is_thread_received_sewing:
-				multi_color_dashboard.is_thread_received_sewing,
-			remarks: multi_color_dashboard.remarks,
-		})
-		.from(multi_color_dashboard)
-		.leftJoin(
-			materialSchema.info,
-			eq(multi_color_dashboard.coil_uuid, materialSchema.info.uuid)
-		)
-		.leftJoin(
-			sql`zipper.v_order_details`,
-			eq(
-				multi_color_dashboard.order_description_uuid,
-				sql`v_order_details.order_description_uuid`
-			)
-		)
-		.where(eq(multi_color_dashboard.uuid, req.params.uuid));
+	const query = sql`
+		SELECT
+			mcd.uuid,
+			mcd.order_description_uuid,
+			vodf.order_number,
+			vodf.item_description,
+			mcd.expected_tape_quantity::float8 AS expected_tape_quantity,
+			mcd.is_swatch_approved,
+			mcd.tape_quantity,
+			mcd.coil_uuid,
+			mi.name AS coil_name,
+			mcd.coil_quantity::float8 AS coil_quantity,
+			mcd.thread_name,
+			mcd.thread_quantity::float8,
+			mcd.is_coil_received_sewing,
+			mcd.is_thread_received_sewing,
+			mcd.remarks
+		FROM
+			zipper.multi_color_dashboard mcd
+		LEFT JOIN
+			material.info mi ON mcd.coil_uuid = mi.uuid
+		LEFT JOIN
+			zipper.v_order_details_full vodf ON mcd.order_description_uuid = vodf.order_description_uuid
+		WHERE
+			mcd.uuid = ${req.params.uuid};
+	`;
+
+	const resultPromise = db.execute(query);
 
 	try {
 		const data = await resultPromise;
@@ -197,7 +156,7 @@ export async function select(req, res, next) {
 			message: 'multi_color_dashboard',
 		};
 
-		res.status(200).json({ toast, data: data[0] });
+		res.status(200).json({ toast, data: data?.rows[0] });
 	} catch (error) {
 		await handleError({ error, res });
 	}
