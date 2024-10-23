18387a3f6938455f5498e6005ab3a75157160a53
rashikbuksh <rashikbuksh@gmail.com>
2024-10-23 16:17:50 +0600

    Refactor zipper_sfg_after_batch_received.sql to update dying_and_iron_prod based on production_quantity_in_kg

--- src/db/trigger/zipper_sfg_after_batch_received.sql
+++ src/db/trigger/zipper_sfg_after_batch_received.sql
@@ -4,7 +4,7 @@ BEGIN
 
     UPDATE zipper.sfg
     SET
-        dying_and_iron_prod = dying_and_iron_prod + CASE WHEN (NEW.received = 1 AND OLD.received = 0) THEN be.quantity ELSE 0 END - CASE WHEN (NEW.received = 0 AND OLD.received = 1) THEN be.quantity ELSE 0 END
+        dying_and_iron_prod = dying_and_iron_prod + CASE WHEN (NEW.received = 1 AND OLD.received = 0) THEN be.production_quantity_in_kg ELSE 0 END - CASE WHEN (NEW.received = 0 AND OLD.received = 1) THEN be.production_quantity_in_kg ELSE 0 END
     FROM zipper.batch_entry be
     WHERE
          zipper.sfg.uuid = be.sfg_uuid AND be.batch_uuid = NEW.uuid;
