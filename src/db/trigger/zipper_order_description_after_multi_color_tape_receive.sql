0545c9dc500699a58c756c2a6862737819ce9e0f
rashikbuksh <rashikbuksh@gmail.com>
2024-10-23 15:22:15 +0600

    Refactor trigger functions for multi_color_tape_receive in zipper_order_description_after_multi_color_tape_receive.sql

---
+++ src/db/trigger/zipper_order_description_after_multi_color_tape_receive.sql
@@ -0,0 +1,59 @@
+-- * inserted 
+CREATE OR REPLACE FUNCTION zipper.order_description_after_multi_color_tape_receive_insert() RETURNS TRIGGER AS $$
+
+BEGIN
+    -- Update order_description
+    UPDATE zipper.order_description
+    SET
+        tape_received = tape_received + NEW.quantity
+    WHERE order_description.uuid = NEW.order_description_uuid;
+
+    RETURN NEW;
+END;
+
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION zipper.order_description_after_multi_color_tape_receive_update() RETURNS TRIGGER AS $$
+BEGIN
+    -- Update order_description
+    UPDATE zipper.order_description
+    SET
+        tape_received = tape_received - OLD.quantity + NEW.quantity
+    WHERE order_description.uuid = NEW.order_description_uuid;
+
+    RETURN NEW;
+END;
+
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION zipper.order_description_after_multi_color_tape_receive_delete() RETURNS TRIGGER AS $$
+BEGIN
+    -- Update order_description
+    UPDATE zipper.order_description
+    SET
+        tape_received = tape_received - OLD.quantity
+    WHERE order_description.uuid = OLD.order_description_uuid;
+
+    RETURN OLD;
+END;
+
+$$ LANGUAGE plpgsql;
+
+-- Trigger for multi_color_tape_receive
+
+
+CREATE OR REPLACE TRIGGER order_description_after_multi_color_tape_receive_insert_trigger
+AFTER INSERT ON zipper.multi_color_tape_receive
+FOR EACH ROW
+EXECUTE FUNCTION zipper.order_description_after_multi_color_tape_receive_insert();
+
+CREATE OR REPLACE TRIGGER order_description_after_multi_color_tape_receive_update_trigger
+AFTER UPDATE ON zipper.multi_color_tape_receive
+FOR EACH ROW
+EXECUTE FUNCTION zipper.order_description_after_multi_color_tape_receive_update();
+
+CREATE OR REPLACE TRIGGER order_description_after_multi_color_tape_receive_delete_trigger
+AFTER DELETE ON zipper.multi_color_tape_receive
+FOR EACH ROW
+EXECUTE FUNCTION zipper.order_description_after_multi_color_tape_receive_delete();
+
