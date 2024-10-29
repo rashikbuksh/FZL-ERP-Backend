23ae5ed6bae60fd510ac787953b8789f16fecb9b
Rashik Buksh <rashikbuksh@gmail.com>
2024-10-29 15:35:26 +0600

    Refactor challan.js to update balance_quantity and max_quantity calculation

--- src/db/thread/query/challan.js
+++ src/db/thread/query/challan.js
@@ -233,8 +233,8 @@ export async function selectByOrderInfoUuid(req, res, next) {
 			toe.delivered::float8 as delivered,
 			toe.warehouse::float8 as warehouse,
 			toe.bleaching as bleaching,
-			(toe.quantity::float8 - toe.warehouse::float8 - toe.delivered::float8)::float8 as balance_quantity,
-			(toe.quantity::float8 - toe.warehouse::float8 - toe.delivered::float8)::float8 as max_quantity,
+			(toe.quantity::float8 - toe.delivered::float8)::float8 as balance_quantity,
+			(toe.quantity::float8 - toe.delivered::float8)::float8 as max_quantity,
 			false as is_checked,
 			0 as short_quantity,
 			0 as reject_quantity
