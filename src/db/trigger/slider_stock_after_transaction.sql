f4ac778e8efb906e3f75b40a9812218a19f2fa2e
Rashik Buksh <rashikbuksh@gmail.com>
2024-10-30 17:57:15 +0600

    Update slider stock trigger to include weight calculations for production

--- src/db/trigger/slider_stock_after_transaction.sql
+++ src/db/trigger/slider_stock_after_transaction.sql
@@ -5,8 +5,12 @@ BEGIN
     SET
         sa_prod = sa_prod - CASE WHEN NEW.from_section = 'sa_prod' THEN NEW.trx_quantity ELSE 0 END
         + CASE WHEN NEW.to_section = 'sa_prod' THEN NEW.trx_quantity ELSE 0 END,
+        sa_prod_weight = sa_prod_weight - CASE WHEN NEW.from_section = 'sa_prod' THEN NEW.weight ELSE 0 END
+        + CASE WHEN NEW.to_section = 'sa_prod' THEN NEW.weight ELSE 0 END,
         coloring_stock = coloring_stock - CASE WHEN NEW.from_section = 'coloring_stock' THEN NEW.trx_quantity ELSE 0 END
-        + CASE WHEN NEW.to_section = 'coloring_stock' THEN NEW.trx_quantity ELSE 0 END
+        + CASE WHEN NEW.to_section = 'coloring_stock' THEN NEW.trx_quantity ELSE 0 END,
+        coloring_stock_weight = coloring_stock_weight - CASE WHEN NEW.from_section = 'coloring_stock' THEN NEW.weight ELSE 0 END
+        + CASE WHEN NEW.to_section = 'coloring_stock' THEN NEW.weight ELSE 0 END
     WHERE uuid = NEW.stock_uuid;
 
     IF NEW.from_section = 'coloring_prod' AND NEW.to_section = 'trx_to_finishing'
@@ -14,7 +18,9 @@ BEGIN
         UPDATE slider.stock
         SET
         coloring_prod = coloring_prod - NEW.trx_quantity,
-        trx_to_finishing = trx_to_finishing + NEW.trx_quantity
+        coloring_prod_weight = coloring_prod_weight - NEW.weight,
+        trx_to_finishing = trx_to_finishing + NEW.trx_quantity,
+        trx_to_finishing_weight = trx_to_finishing_weight + NEW.weight
         WHERE uuid = NEW.stock_uuid;
 
         UPDATE zipper.order_description
@@ -27,7 +33,8 @@ BEGIN
     THEN
         UPDATE slider.stock
         SET
-            coloring_stock = coloring_stock + CASE WHEN NEW.to_section = 'assembly_stock_to_coloring_stock' THEN NEW.trx_quantity ELSE 0 END
+            coloring_stock = coloring_stock + CASE WHEN NEW.to_section = 'assembly_stock_to_coloring_stock' THEN NEW.trx_quantity ELSE 0 END,
+            coloring_stock_weight = coloring_stock_weight + CASE WHEN NEW.to_section = 'assembly_stock_to_coloring_stock' THEN NEW.weight ELSE 0 END
         WHERE uuid = NEW.stock_uuid;
 
         UPDATE slider.assembly_stock
@@ -47,10 +54,18 @@ BEGIN
     --update slider.stock table
     UPDATE slider.stock
     SET
-        sa_prod = sa_prod + CASE WHEN OLD.from_section = 'sa_prod' THEN OLD.trx_quantity ELSE 0 END
-        - CASE WHEN OLD.to_section = 'sa_prod' THEN OLD.trx_quantity ELSE 0 END,
-        coloring_stock = coloring_stock + CASE WHEN OLD.from_section = 'coloring_stock' THEN OLD.trx_quantity ELSE 0 END
-        - CASE WHEN OLD.to_section = 'coloring_stock' THEN OLD.trx_quantity ELSE 0 END
+        sa_prod = sa_prod 
+            + CASE WHEN OLD.from_section = 'sa_prod' THEN OLD.trx_quantity ELSE 0 END
+            - CASE WHEN OLD.to_section = 'sa_prod' THEN OLD.trx_quantity ELSE 0 END,
+        sa_prod_weight = sa_prod_weight 
+            + CASE WHEN OLD.from_section = 'sa_prod' THEN OLD.weight ELSE 0 END
+            - CASE WHEN OLD.to_section = 'sa_prod' THEN OLD.weight ELSE 0 END,
+        coloring_stock = coloring_stock 
+            + CASE WHEN OLD.from_section = 'coloring_stock' THEN OLD.trx_quantity ELSE 0 END
+            - CASE WHEN OLD.to_section = 'coloring_stock' THEN OLD.trx_quantity ELSE 0 END,
+        coloring_stock_weight = coloring_stock_weight
+            + CASE WHEN OLD.from_section = 'coloring_stock' THEN OLD.weight ELSE 0 END
+            - CASE WHEN OLD.to_section = 'coloring_stock' THEN OLD.weight ELSE 0 END
     WHERE uuid = OLD.stock_uuid;
 
     IF OLD.from_section = 'coloring_prod' AND OLD.to_section = 'trx_to_finishing'
@@ -58,7 +73,9 @@ BEGIN
         UPDATE slider.stock
         SET
         coloring_prod = coloring_prod + OLD.trx_quantity,
-        trx_to_finishing = trx_to_finishing - OLD.trx_quantity
+        coloring_prod_weight = coloring_prod_weight + OLD.weight,
+        trx_to_finishing = trx_to_finishing - OLD.trx_quantity,
+        trx_to_finishing_weight = trx_to_finishing_weight - OLD.weight
         WHERE uuid = OLD.stock_uuid;
 
         UPDATE zipper.order_description
@@ -72,7 +89,8 @@ BEGIN
     THEN
         UPDATE slider.stock
         SET
-            coloring_stock = coloring_stock - CASE WHEN OLD.to_section = 'assembly_stock_to_coloring_stock' THEN OLD.trx_quantity ELSE 0 END
+            coloring_stock = coloring_stock - CASE WHEN OLD.to_section = 'assembly_stock_to_coloring_stock' THEN OLD.trx_quantity ELSE 0 END,
+            coloring_stock_weight = coloring_stock_weight - CASE WHEN OLD.to_section = 'assembly_stock_to_coloring_stock' THEN OLD.weight ELSE 0 END
         WHERE uuid = OLD.stock_uuid;
 
         UPDATE slider.assembly_stock
@@ -97,11 +115,21 @@ BEGIN
         + CASE WHEN NEW.to_section = 'sa_prod' THEN NEW.trx_quantity ELSE 0 END
         + CASE WHEN OLD.from_section = 'sa_prod' THEN OLD.trx_quantity ELSE 0 END
         - CASE WHEN OLD.to_section = 'sa_prod' THEN OLD.trx_quantity ELSE 0 END,
+        sa_prod_weight = sa_prod_weight
+        - CASE WHEN NEW.from_section = 'sa_prod' THEN NEW.weight ELSE 0 END
+        + CASE WHEN NEW.to_section = 'sa_prod' THEN NEW.weight ELSE 0 END
+        + CASE WHEN OLD.from_section = 'sa_prod' THEN OLD.weight ELSE 0 END
+        - CASE WHEN OLD.to_section = 'sa_prod' THEN OLD.weight ELSE 0 END,
         coloring_stock = coloring_stock 
         - CASE WHEN NEW.from_section = 'coloring_stock' THEN NEW.trx_quantity ELSE 0 END
         + CASE WHEN NEW.to_section = 'coloring_stock' THEN NEW.trx_quantity ELSE 0 END
         + CASE WHEN OLD.from_section = 'coloring_stock' THEN OLD.trx_quantity ELSE 0 END
-        - CASE WHEN OLD.to_section = 'coloring_stock' THEN OLD.trx_quantity ELSE 0 END
+        - CASE WHEN OLD.to_section = 'coloring_stock' THEN OLD.trx_quantity ELSE 0 END,
+        coloring_stock_weight = coloring_stock_weight
+        - CASE WHEN NEW.from_section = 'coloring_stock' THEN NEW.weight ELSE 0 END
+        + CASE WHEN NEW.to_section = 'coloring_stock' THEN NEW.weight ELSE 0 END
+        + CASE WHEN OLD.from_section = 'coloring_stock' THEN OLD.weight ELSE 0 END
+        - CASE WHEN OLD.to_section = 'coloring_stock' THEN OLD.weight ELSE 0 END
     WHERE uuid = NEW.stock_uuid;
 
     IF NEW.from_section = 'coloring_prod' AND NEW.to_section = 'trx_to_finishing'
@@ -109,7 +137,9 @@ BEGIN
         UPDATE slider.stock
         SET
         coloring_prod = coloring_prod - NEW.trx_quantity + OLD.trx_quantity,
-        trx_to_finishing = trx_to_finishing + NEW.trx_quantity - OLD.trx_quantity
+        coloring_prod_weight = coloring_prod_weight - NEW.weight + OLD.weight,
+        trx_to_finishing = trx_to_finishing + NEW.trx_quantity - OLD.trx_quantity,
+        trx_to_finishing_weight = trx_to_finishing_weight + NEW.weight - OLD.weight
         WHERE uuid = NEW.stock_uuid;
 
         UPDATE zipper.order_description
@@ -125,7 +155,9 @@ BEGIN
         UPDATE slider.stock
         SET
             coloring_stock = coloring_stock 
-            - CASE WHEN OLD.to_section = 'assembly_stock_to_coloring_stock' THEN OLD.trx_quantity ELSE 0 END
+            - CASE WHEN OLD.to_section = 'assembly_stock_to_coloring_stock' THEN OLD.trx_quantity ELSE 0 END,
+            coloring_stock_weight = coloring_stock_weight
+            - CASE WHEN OLD.to_section = 'assembly_stock_to_coloring_stock' THEN OLD.weight ELSE 0 END
         WHERE uuid = OLD.stock_uuid;
 
         UPDATE slider.assembly_stock
@@ -142,7 +174,8 @@ BEGIN
     THEN
         UPDATE slider.stock
         SET
-            coloring_stock = coloring_stock + CASE WHEN NEW.to_section = 'assembly_stock_to_coloring_stock' THEN NEW.trx_quantity ELSE 0 END
+            coloring_stock = coloring_stock + CASE WHEN NEW.to_section = 'assembly_stock_to_coloring_stock' THEN NEW.trx_quantity ELSE 0 END,
+            coloring_stock_weight = coloring_stock_weight + CASE WHEN NEW.to_section = 'assembly_stock_to_coloring_stock' THEN NEW.weight ELSE 0 END
         WHERE uuid = NEW.stock_uuid;
 
         UPDATE slider.assembly_stock
