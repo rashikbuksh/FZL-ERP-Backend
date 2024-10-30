8667e644b9892cca51a3e445a032a82292ee0579
rashikbuksh <rashikbuksh@gmail.com>
2024-10-30 23:17:55 +0600

    Enhance value calculation in selectPiEntryByPiUuid query to account for order_type

--- src/db/commercial/query/pi_cash_entry.js
+++ src/db/commercial/query/pi_cash_entry.js
@@ -184,7 +184,14 @@ export async function selectPiEntryByPiUuid(req, res, next) {
 					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN oe.party_price::float8 ELSE toe.party_price::float8 END as unit_price,
 					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN ROUND(oe.party_price/12, 4)::float8 ELSE toe.party_price::float8 END as unit_price_pcs,
 					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN sfg.pi::float8 ELSE toe.pi::float8 END as given_pi_cash_quantity,
-					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN ROUND(pe.pi_cash_quantity * (oe.party_price/12), 4)::float8 ELSE ROUND(pe.pi_cash_quantity * toe.party_price, 4)::float8 END as value,
+					CASE WHEN pe.thread_order_entry_uuid IS NULL 
+						THEN CASE 
+							WHEN vodf.order_type = 'tape' 
+								THEN ROUND(pe.pi_cash_quantity * (oe.party_price), 4)::float8 
+								ELSE ROUND(pe.pi_cash_quantity * oe.party_price/12, 4)::float8 
+							END 
+						ELSE ROUND(pe.pi_cash_quantity * toe.party_price, 4)::float8 
+					END as value,
 					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN ROUND((pe.pi_cash_quantity/12) * oe.party_price, 4)::float8 ELSE ROUND(pe.pi_cash_quantity * toe.party_price, 4)::float8 END as value_dzn,
 					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN (oe.quantity - sfg.pi)::float8 ELSE (toe.quantity - toe.pi)::float8 END as balance_quantity,
 					pe.thread_order_entry_uuid as thread_order_entry_uuid,
