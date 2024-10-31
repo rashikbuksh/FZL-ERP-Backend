c17a899921261a9e01db04a4eefa0c2095464759
Rashik Buksh <rashikbuksh@gmail.com>
2024-10-31 13:09:19 +0600

    Enhance value calculation in selectPiEntryByPiUuid query to handle 'tape' order_type and improve accuracy

--- src/db/commercial/query/pi_cash_entry.js
+++ src/db/commercial/query/pi_cash_entry.js
@@ -185,7 +185,11 @@ export async function selectPiEntryByPiUuid(req, res, next) {
 					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN ROUND(oe.party_price/12, 4)::float8 ELSE toe.party_price::float8 END as unit_price_pcs,
 					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN sfg.pi::float8 ELSE toe.pi::float8 END as given_pi_cash_quantity,
 					CASE WHEN pe.thread_order_entry_uuid IS NULL 
-						THEN ROUND(pe.pi_cash_quantity * oe.party_price/12, 4)::float8 
+						THEN 
+							CASE WHEN vodf.order_type = 'tape' 
+								THEN oe.size::float8 * (oe.party_price::float8)::float8 
+								ELSE ROUND(pe.pi_cash_quantity * oe.party_price/12, 4)::float8 
+							END
 						ELSE ROUND(pe.pi_cash_quantity * toe.party_price, 4)::float8 
 					END as value,
 					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN ROUND((pe.pi_cash_quantity/12) * oe.party_price, 4)::float8 ELSE ROUND(pe.pi_cash_quantity * toe.party_price, 4)::float8 END as value_dzn,
