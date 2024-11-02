6e108bcdb49c44fddf1187a5a5554a4266f3b173
Rashik Buksh <rashikbuksh@gmail.com>
2024-11-02 14:55:08 +0600

    Enhance stock query to include transaction quantities and adjust filtering logic for swatch_approved_quantity

--- src/db/others/query/query.js
+++ src/db/others/query/query.js
@@ -1149,8 +1149,20 @@ export async function selectSliderStockWithOrderDescription(req, res, next) {
 		slider.stock
 	LEFT JOIN
 		zipper.v_order_details_full vodf ON stock.order_description_uuid = vodf.order_description_uuid
+	LEFT JOIN 
+		(
+			SELECT
+				stock.uuid AS stock_uuid,
+				SUM(transaction.trx_quantity)::float8 AS trx_quantity
+			FROM
+				slider.transaction
+			LEFT JOIN
+				slider.stock ON transaction.stock_uuid = stock.uuid
+			GROUP BY
+				stock.uuid
+		) AS slider_transaction_given ON stock.uuid = slider_transaction_given.stock_uuid
 	WHERE 
-		stock.swatch_approved_quantity > 0;
+		stock.swatch_approved_quantity > 0 AND (stock.swatch_approved_quantity - COALESCE(slider_transaction_given.trx_quantity, 0)) <= stock.swatch_approved_quantity
 		`;
 
 	const stockPromise = db.execute(query);
