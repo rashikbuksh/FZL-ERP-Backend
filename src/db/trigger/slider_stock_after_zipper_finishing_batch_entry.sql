7fae0b43183cf630a456374a227c40a7d20f5597
Jobayer Hossen <jobayerhossen64@gmail.com>
2024-11-11 11:52:02 +0600

    Add trigger to update stock quantity after deleting finishing batch entry

---
+++ src/db/trigger/slider_stock_after_zipper_finishing_batch_entry.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE FUNCTION slider.stock_after_zipper_finishing_batch_entry_delete_funct () RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE slider.stock
+    SET
+        batch_quantity = batch_quantity - OLD.quantity
+    WHERE 
+        uuid = (SELECT finishing_batch_uuid 
+                FROM zipper.finishing_batch_entry 
+                WHERE uuid = OLD.finishing_batch_entry_uuid);
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE TRIGGER stock_after_zipper_finishing_batch_entry_delete
+AFTER DELETE ON zipper.finishing_batch_entry
+FOR EACH ROW
+EXECUTE FUNCTION slider.stock_after_zipper_finishing_batch_entry_delete_funct();
+
