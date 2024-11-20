f8f795b42ac8b8a692dae3377ac70b4c2166da1c
Jobayer Hossen <jobayerhossen64@gmail.com>
2024-11-21 01:47:26 +0600

    Refactor delivery_challan_after_packing_list trigger to improve query structure and update conditions

--- src/db/trigger/delivery_challan_after_packing_list.sql
+++ src/db/trigger/delivery_challan_after_packing_list.sql
@@ -5,18 +5,20 @@ DECLARE
     challan_uuid_gp uuid;
 BEGIN
     SELECT 
-        challan_uuid INTO challan_uuid_gp 
+        uuid 
+    INTO 
+        challan_uuid_gp 
     FROM 
-        delivery.packing_list 
+        delivery.challan 
     WHERE 
-        challan_uuid = NEW.challan_uuid;
+       uuid = NEW.challan_uuid;
 
     IF (SELECT 
             COUNT(*) 
         FROM 
             delivery.packing_list 
         WHERE 
-            delivery.packing_list.challan_uuid = challan_uuid_gp AND delivery.packing_list.gate_pass != 1) = 0 THEN
+            challan_uuid = challan_uuid_gp AND gate_pass != 1) = 0 THEN
 
         UPDATE 
             delivery.challan
@@ -28,9 +30,10 @@ BEGIN
 
     RETURN NEW;
 END;
+
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE TRIGGER delivery_challan_after_packing_list_update
-AFTER INSERT OR UPDATE ON delivery.packing_list
+AFTER  UPDATE ON delivery.packing_list
 FOR EACH ROW
 EXECUTE FUNCTION delivery.delivery_challan_after_packing_list_update_funct();
