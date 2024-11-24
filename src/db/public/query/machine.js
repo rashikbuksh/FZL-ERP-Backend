6aac77106e299697a185d8efdc278f5f43c1b075
Jobayer Hossen <jobayerhossen64@gmail.com>
2024-11-24 13:51:27 +0600

    Add endpoint and query to retrieve machines by production date for enhanced reporting

--- src/db/public/query/machine.js
+++ src/db/public/query/machine.js
@@ -1,10 +1,8 @@
 import { desc, eq, sql } from 'drizzle-orm';
 import { createApi } from '../../../util/api.js';
-import {
-	handleError,
-	validateRequest,
-} from '../../../util/index.js';
+import { handleError, validateRequest } from '../../../util/index.js';
 import * as hrSchema from '../../hr/schema.js';
+import * as zipperSchema from '../../zipper/schema.js';
 import db from '../../index.js';
 import { decimalToNumber } from '../../variables.js';
 import { machine } from '../../public/schema.js';
@@ -148,6 +146,68 @@ export async function select(req, res, next) {
 		};
 
 		return await res.status(200).json({ toast, data: data[0] });
+	} catch (error) {
+		await handleError({ error, res });
+	}
+}
+
+export async function selectByDate(req, res, next) {
+	if (!(await validateRequest(req, next))) return;
+	console.log('req.params.date', req.params.date);
+
+	const query = sql`
+        SELECT
+            DATE(zdb.production_date) as date,
+            pm.name AS machine_name,
+            zdb.slot,
+            CONCAT('B', to_char(zdb.created_at, 'YY'), '-', LPAD(zdb.id::text, 4, '0')) AS batch_no,
+            vodf.order_number,
+            zoe.color,
+            zbe.production_quantity_in_kg::float8 as weight
+        FROM public.machine pm
+        LEFT JOIN zipper.dyeing_batch zdb ON zdb.machine_uuid = pm.uuid
+        LEFT JOIN zipper.dyeing_batch_entry zbe ON zbe.dyeing_batch_uuid = zdb.uuid
+        LEFT JOIN zipper.sfg sfg ON sfg.uuid = zbe.sfg_uuid
+        LEFT JOIN zipper.order_entry zoe ON zoe.uuid = sfg.order_entry_uuid
+        LEFT JOIN zipper.order_description zod ON zod.uuid = zoe.order_description_uuid
+        LEFT JOIN zipper.v_order_details_full vodf ON vodf.order_description_uuid = zod.uuid
+        WHERE DATE(zdb.production_date) = ${req.params.date}
+        ORDER BY zdb.created_at DESC
+    `;
+
+	try {
+		const { rows: results } = await db.execute(query);
+
+		if (!Array.isArray(results)) {
+			throw new TypeError('Expected results to be an array');
+		}
+
+		const groupedResults = results.reduce((acc, item) => {
+			if (!acc[item.machine_name]) {
+				acc[item.machine_name] = [];
+			}
+			acc[item.machine_name].push({
+				slot: item.slot,
+				batch_no: item.batch_no,
+				order_no: item.order_number,
+				color: item.color,
+				weight: item.weight,
+			});
+			return acc;
+		}, {});
+
+		const response = {
+			date: results[0]?.date,
+			...groupedResults,
+		};
+
+		const toast = {
+			status: 200,
+			type: 'select_all',
+			message: 'Machine',
+		};
+
+		return res.status(200).json({ toast, data: response });
 	} catch (error) {
 		await handleError({ error, res });
 	}
