59b317f13c9305800db5ce9b79b7cf9ab6f4dd51
Rashik Buksh <rashikbuksh@gmail.com>
2024-11-26 14:25:31 +0600

    Enhance order info query with conditional join for recipe count and update finishing batch entry logic

--- src/db/others/query/query.js
+++ src/db/others/query/query.js
@@ -388,13 +388,27 @@ export async function selectOrderInfo(req, res, next) {
 			break;
 	}
 
-	const orderInfoPromise = db
+	let orderInfoPromise = db
 		.select({
 			value: zipperSchema.order_info.uuid,
 			label: sql`CONCAT('Z', to_char(order_info.created_at, 'YY'), '-', LPAD(order_info.id::text, 4, '0'))`,
 		})
-		.from(zipperSchema.order_info)
-		.where(filterCondition);
+		.from(zipperSchema.order_info);
+
+	if (is_sample == 'true') {
+		orderInfoPromise = orderInfoPromise.leftJoin(
+			sql`(
+                SELECT COUNT(recipe_uuid) as recipe_count, od.order_info_uuid as order_info_uuid
+                FROM zipper.sfg
+                LEFT JOIN zipper.order_entry oe ON sfg.order_entry_uuid = oe.uuid
+                LEFT JOIN zipper.order_description od ON oe.order_description_uuid = od.uuid
+                GROUP BY od.order_info_uuid
+            ) as sfg_recipe`,
+			sql`${zipperSchema.order_info.uuid} = sfg_recipe.order_info_uuid AND sfg_recipe.recipe_count > 0`
+		);
+	}
+
+	orderInfoPromise = orderInfoPromise.where(filterCondition);
 
 	try {
 		const data = await orderInfoPromise;
