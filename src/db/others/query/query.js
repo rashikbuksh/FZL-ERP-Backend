973c868eceebc88c1e307aab9a04cb5b45e52735
rashikbuksh <rashikbuksh@gmail.com>
2024-12-02 17:02:48 +0600

    Enhance selectOrderNumberForPi query to include order_entry and sfg joins, and update quantity check logic for improved data filtering

--- src/db/others/query/query.js
+++ src/db/others/query/query.js
@@ -1124,12 +1124,14 @@ export async function selectOrderNumberForPi(req, res, next) {
 			FROM
 				zipper.v_order_details vod
 				LEFT JOIN zipper.order_info oi ON vod.order_info_uuid = oi.uuid
+				LEFT JOIN zipper.order_entry oe ON vod.order_description_uuid = oe.order_description_uuid
+				LEFT JOIN zipper.sfg sfg ON oe.uuid = sfg.order_entry_uuid
 			WHERE
 				vod.is_cash = 0 AND
 				vod.marketing_uuid = ${req.params.marketing_uuid} AND
 				oi.party_uuid = ${req.params.party_uuid} AND 
 				oi.is_sample = 0
-				${pi_uuid ? sql`AND vod.order_info_uuid IN (SELECT json_array_elements_text(order_info_uuids::json) FROM commercial.pi_cash WHERE uuid = ${pi_uuid})` : sql``}
+				${pi_uuid ? sql`AND vod.order_info_uuid IN (SELECT json_array_elements_text(order_info_uuids::json) FROM commercial.pi_cash WHERE uuid = ${pi_uuid})` : sql`AND oe.quantity - sfg.pi > 0`}
 			ORDER BY
 				vod.order_number ASC`;
 	}
