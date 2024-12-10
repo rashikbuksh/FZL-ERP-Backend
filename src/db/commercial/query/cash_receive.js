605deb3cb54b57d6f8224cb306b565570f6405b3
Jobayer Hossen <jobayerhossen64@gmail.com>
2024-12-10 13:29:16 +0600

    Refactor selectAll and select functions to use parameterized SQL queries for cash_receive data retrieval

--- src/db/commercial/query/cash_receive.js
+++ src/db/commercial/query/cash_receive.js
@@ -75,7 +75,66 @@ export async function remove(req, res, next) {
 }
 
 export async function selectAll(req, res, next) {
-	const cashReceivePromise = db.select(cash_receive).from(cash_receive);
+	if (!(await validateRequest(req, next))) return;
+	const query = sql`
+				SELECT
+					cash_receive.uuid,
+					cash_receive.pi_cash_uuid,
+					CASE 
+						WHEN pi_cash.is_pi = 1 THEN CONCAT('PI', to_char(pi_cash.created_at, 'YY'), '-', LPAD(pi_cash.id::text, 4, '0')) 
+						ELSE CONCAT('CI', to_char(pi_cash.created_at, 'YY'), '-', LPAD(pi_cash.id::text, 4, '0')) 
+					END AS pi_cash_id,
+					pi_cash.receive_amount::float8,
+					cash_receive.amount::float8,
+					CASE 
+						WHEN pi_cash.is_pi = 1 
+						THEN ROUND((total_pi_amount.total_amount::numeric + total_pi_amount_thread.total_amount::numeric), 2) 
+						ELSE ROUND((total_pi_amount.total_amount::numeric + total_pi_amount_thread.total_amount::numeric), 2) * pi_cash.conversion_rate::float8 END AS total_amount,
+					cash_receive.created_by,
+					hr.users.name AS created_by_name,
+					cash_receive.created_at,
+					cash_receive.updated_at,
+					cash_receive.remarks
+				FROM
+					commercial.cash_receive
+				LEFT JOIN
+					hr.users ON cash_receive.created_by = hr.users.uuid
+				LEFT JOIN
+					commercial.pi_cash ON cash_receive.pi_cash_uuid = commercial.pi_cash.uuid
+				LEFT JOIN
+					(
+						SELECT 
+							(
+								SUM(
+									coalesce(pi_cash_entry.pi_cash_quantity,0)  * coalesce(order_entry.party_price,0)
+								)
+							)::float8 as total_amount, 
+							pi_cash.uuid as pi_cash_uuid
+						FROM commercial.pi_cash 
+							LEFT JOIN commercial.pi_cash_entry ON pi_cash.uuid = pi_cash_entry.pi_cash_uuid 
+							LEFT JOIN thread.order_entry ON pi_cash_entry.thread_order_entry_uuid = order_entry.uuid 
+						GROUP BY pi_cash.uuid
+					) as total_pi_amount ON total_pi_amount.pi_cash_uuid = cash_receive.pi_cash_uuid
+				LEFT JOIN 
+				(
+					SELECT 
+						(
+							SUM(
+								coalesce(pi_cash_entry.pi_cash_quantity,0)  * coalesce(order_entry.party_price,0)
+							)
+						)::float8 as total_amount, 
+						pi_cash.uuid as pi_cash_uuid
+					FROM commercial.pi_cash 
+						LEFT JOIN commercial.pi_cash_entry ON pi_cash.uuid = pi_cash_entry.pi_cash_uuid 
+						LEFT JOIN thread.order_entry ON pi_cash_entry.thread_order_entry_uuid = order_entry.uuid 
+					GROUP BY pi_cash.uuid
+
+				) as total_pi_amount_thread ON total_pi_amount_thread.pi_cash_uuid = cash_receive.pi_cash_uuid
+			ORDER BY cash_receive.created_at DESC
+	`;
+	// const cashReceivePromise = db.select(cash_receive).from(cash_receive);
+
+	const cashReceivePromise = db.query(query);
 
 	try {
 		const data = await cashReceivePromise;
@@ -93,10 +152,67 @@ export async function selectAll(req, res, next) {
 export async function select(req, res, next) {
 	if (!(await validateRequest(req, next))) return;
 
-	const cashReceivePromise = db
-		.select(cash_receive)
-		.from(cash_receive)
-		.where(eq(cash_receive.uuid, req.params.uuid));
+	// const cashReceivePromise = db
+	// 	.select(cash_receive)
+	// 	.from(cash_receive)
+	// 	.where(eq(cash_receive.uuid, req.params.uuid));
+
+	const query = sql`
+				SELECT
+					cash_receive.uuid,
+					cash_receive.pi_cash_uuid,
+					CASE 
+						WHEN pi_cash.is_pi = 1 THEN CONCAT('PI', to_char(pi_cash.created_at, 'YY'), '-', LPAD(pi_cash.id::text, 4, '0')) 
+						ELSE CONCAT('CI', to_char(pi_cash.created_at, 'YY'), '-', LPAD(pi_cash.id::text, 4, '0')) 
+					END AS pi_cash_id,
+					pi_cash.receive_amount::float8,
+					cash_receive.amount::float8,
+					CASE 
+						WHEN pi_cash.is_pi = 1 
+						THEN ROUND((total_pi_amount.total_amount::numeric + total_pi_amount_thread.total_amount::numeric), 2) 
+						ELSE ROUND((total_pi_amount.total_amount::numeric + total_pi_amount_thread.total_amount::numeric), 2) * pi_cash.conversion_rate::float8 END AS total_amount,
+					cash_receive.created_by,
+					hr.users.name AS created_by_name,
+					cash_receive.created_at,
+					cash_receive.updated_at,
+					cash_receive.remarks
+				FROM
+					commercial.cash_receive
+				LEFT JOIN
+					hr.users ON cash_receive.created_by = hr.users.uuid
+				LEFT JOIN
+					commercial.pi_cash ON cash_receive.pi_cash_uuid = commercial.pi_cash.uuid
+				LEFT JOIN
+					(
+						SELECT 
+							(
+								SUM(
+									coalesce(pi_cash_entry.pi_cash_quantity,0)  * coalesce(order_entry.party_price,0)
+								)
+							)::float8 as total_amount, 
+							pi_cash.uuid as pi_cash_uuid
+						FROM commercial.pi_cash 
+							LEFT JOIN commercial.pi_cash_entry ON pi_cash.uuid = pi_cash_entry.pi_cash_uuid 
+							LEFT JOIN thread.order_entry ON pi_cash_entry.thread_order_entry_uuid = order_entry.uuid 
+						GROUP BY pi_cash.uuid
+					) as total_pi_amount ON total_pi_amount.pi_cash_uuid = cash_receive.pi_cash_uuid
+				LEFT JOIN
+				(
+					SELECT 
+						(
+							SUM(
+								coalesce(pi_cash_entry.pi_cash_quantity,0)  * coalesce(order_entry.party_price,0)
+							)
+						)::float8 as total_amount, 
+						pi_cash.uuid as pi_cash_uuid
+					FROM commercial.pi_cash 
+						LEFT JOIN commercial.pi_cash_entry ON pi_cash.uuid = pi_cash_entry.pi_cash_uuid 
+						LEFT JOIN thread.order_entry ON pi_cash_entry.thread_order_entry_uuid = order_entry.uuid 
+					GROUP BY pi_cash.uuid
+
+				) as total_pi_amount_thread ON total_pi_amount_thread.pi_cash_uuid = cash_receive.pi_cash_uuid
+				WHERE cash_receive.uuid = ${req.params.uuid}
+	`;
 
 	try {
 		const data = await cashReceivePromise;
