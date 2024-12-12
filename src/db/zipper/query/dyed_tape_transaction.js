8e2c2d501522c95a7551c18e0415d3fbafd09888
Jobayer Hossen <jobayerhossen64@gmail.com>
2024-12-12 15:22:57 +0600

    Update dyed_tape_transaction query to use v_order_details_full and include stock calculations

--- src/db/zipper/query/dyed_tape_transaction.js
+++ src/db/zipper/query/dyed_tape_transaction.js
@@ -123,9 +123,9 @@ export async function select(req, res, next) {
 		SELECT
 			dtt.uuid AS uuid,
 			dtt.order_description_uuid AS order_description_uuid,
-			vod.order_number as order_number,
-			vod.item_description as item_description,
-			vod.order_type,
+			vodf.order_number as order_number,
+			vodf.item_description as item_description,
+			vodf.order_type,
 			dtt.colors as colors,
 			dtt.trx_quantity::float8 AS trx_quantity,
 			dtt.created_by AS created_by,
@@ -137,12 +137,23 @@ export async function select(req, res, next) {
 			oe.color,
 			oe.style,
 			CONCAT(oe.color, ' - ', oe.style) as color_style,
-			dtt.trx_quantity_in_meter::float8
+			dtt.trx_quantity_in_meter::float8,
+			CASE WHEN vodf.is_multi_color = 1 THEN vodf.multi_color_tape_received ELSE coalesce(batch_stock.stock,0)::float8 END as stock,
+			CASE WHEN vodf.is_multi_color = 1 THEN vodf.multi_color_tape_received ELSE coalesce(batch_stock.stock,0)::float8 END + dtt.trx_quantity::float8 as max_trx_quantity
 		FROM zipper.dyed_tape_transaction dtt
 			LEFT JOIN hr.users u ON dtt.created_by = u.uuid
-			LEFT JOIN zipper.v_order_details vod ON dtt.order_description_uuid = vod.order_description_uuid
+			LEFT JOIN zipper.v_order_details_full vodf ON dtt.order_description_uuid = vodf.order_description_uuid
 			LEFT JOIN zipper.sfg sfg ON dtt.sfg_uuid = sfg.uuid
 			LEFT JOIN zipper.order_entry oe ON sfg.order_entry_uuid = oe.uuid
+			LEFT JOIN (
+					SELECT oe.order_description_uuid, SUM(be.production_quantity_in_kg) as stock
+					FROM zipper.order_entry oe
+						LEFT JOIN zipper.sfg ON oe.uuid = sfg.order_entry_uuid
+						LEFT JOIN zipper.dyeing_batch_entry be ON be.sfg_uuid = sfg.uuid
+						LEFT JOIN zipper.dyeing_batch b ON b.uuid = be.dyeing_batch_uuid
+					WHERE b.received = 1
+					GROUP BY oe.order_description_uuid
+				) batch_stock ON vodf.order_description_uuid = batch_stock.order_description_uuid
 		WHERE dtt.uuid = ${req.params.uuid}
 	`;
 
