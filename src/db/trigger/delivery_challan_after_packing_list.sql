ed5becbd68d46a2e3d73ddfe1d0b7eed3841e523
rashikbuksh <rashikbuksh@gmail.com>
2024-12-20 00:31:40 +0600

    Refactor delivery trigger and query to handle is_warehouse_received flag correctly for packing list entries

--- src/db/trigger/delivery_challan_after_packing_list.sql
+++ src/db/trigger/delivery_challan_after_packing_list.sql
@@ -165,33 +165,33 @@ AFTER UPDATE ON delivery.packing_list
 FOR EACH ROW
 EXECUTE FUNCTION delivery.delivery_challan_after_packing_list_update_funct();
 
-CREATE OR REPLACE FUNCTION delivery.delivery_challan_after_packing_list_insert_funct()
+CREATE OR REPLACE FUNCTION delivery.delivery_challan_after_packing_list_entry_insert_funct()
 RETURNS TRIGGER AS $$
 DECLARE
     item_for_gp TEXT;
+    is_warehouse_received_gp BOOLEAN;
 BEGIN
     -- For Challan And zipper, thread
     SELECT
-        pl.item_for INTO item_for_gp
+        pl.item_for, pl.is_warehouse_received INTO item_for_gp, is_warehouse_received_gp
     FROM
         delivery.packing_list pl
     WHERE
-        pl.uuid = NEW.uuid;
+        pl.uuid = NEW.packing_list_uuid;
 
     IF item_for_gp = 'thread' OR item_for_gp = 'sample_thread' THEN
-        IF NEW.is_warehouse_received = TRUE THEN
+        IF is_warehouse_received_gp = TRUE THEN
             UPDATE 
                 thread.order_entry
             SET 
                 warehouse = warehouse + ple.quantity,
                 production_quantity = production_quantity - ple.quantity
             FROM delivery.packing_list_entry ple
-            LEFT JOIN delivery.packing_list pl ON ple.packing_list_uuid = pl.uuid
             WHERE 
-                pl.uuid = NEW.uuid AND ple.thread_order_entry_uuid = order_entry.uuid;
+                order_entry.uuid = NEW.thread_order_entry_uuid AND ple.uuid = NEW.uuid;
         END IF;
     ELSE
-        IF NEW.is_warehouse_received = TRUE THEN
+        IF is_warehouse_received_gp = TRUE THEN
             UPDATE 
                 zipper.sfg
             SET 
@@ -199,15 +199,16 @@ BEGIN
                 finishing_prod = finishing_prod - ple.quantity
             FROM 
                 delivery.packing_list_entry ple
-            LEFT JOIN delivery.packing_list pl ON ple.packing_list_uuid = pl.uuid
             WHERE 
-                pl.uuid = NEW.uuid AND ple.sfg_uuid = sfg.uuid;
+                sfg.uuid = NEW.sfg_uuid AND ple.uuid = NEW.uuid;
         END IF;
     END IF;
+
+    RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE TRIGGER delivery_challan_after_packing_list_insert
-AFTER INSERT ON delivery.packing_list
+CREATE OR REPLACE TRIGGER delivery_challan_after_packing_list_entry_insert
+AFTER INSERT ON delivery.packing_list_entry
 FOR EACH ROW
-EXECUTE FUNCTION delivery.delivery_challan_after_packing_list_insert_funct();
+EXECUTE FUNCTION delivery.delivery_challan_after_packing_list_entry_insert_funct();
