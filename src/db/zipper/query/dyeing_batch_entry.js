f330002985f31ac9050814e68bb4dbe7fb54c478
rashikbuksh <rashikbuksh@gmail.com>
2024-12-23 18:07:23 +0600

    Fix reference to batch_type_enum in selectBatchEntryByBatchUuid query

--- src/db/zipper/query/dyeing_batch_entry.js
+++ src/db/zipper/query/dyeing_batch_entry.js
@@ -191,7 +191,7 @@ export async function selectBatchEntryByBatchUuid(req, res, next) {
 			tc.raw_per_kg_meter::float8 as raw_mtr_per_kg,
 			tc.dyed_per_kg_meter::float8 as dyed_mtr_per_kg,
 			vodf.order_type,
-			be.batch_type_enum as batch_type
+			b.batch_type_enum as batch_type
 		FROM
 			zipper.dyeing_batch_entry be
 		LEFT JOIN
