4fb6ef8da73de470af660b84a0e1060542cbd22a
rashikbuksh <rashikbuksh@gmail.com>
2024-12-29 17:49:14 +0600

    Exclude 'slider' order type from getTapeAssigned query results

--- src/db/zipper/query/order_info.js
+++ src/db/zipper/query/order_info.js
@@ -422,7 +422,7 @@ export async function getTapeAssigned(req, res, next) {
 						FROM zipper.order_entry oe
 						GROUP BY oe.order_description_uuid
 					) order_entry_counts ON vodf.order_description_uuid = order_entry_counts.order_description_uuid
-					WHERE vodf.order_description_uuid IS NOT NULL AND vodf.is_multi_color = 0
+					WHERE vodf.order_description_uuid IS NOT NULL AND vodf.is_multi_color = 0 AND vodf.order_type != 'slider'
 					ORDER BY 
 						vodf.tape_coil_uuid ASC NULLS FIRST,
 						vodf.order_number DESC,
