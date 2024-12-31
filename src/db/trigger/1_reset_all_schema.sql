44965b2548eb87e62b9fffbdfb61f0d327db4066
rashikbuksh <rashikbuksh@gmail.com>
2025-01-01 03:01:17 +0600

    Add function to reset all database sequences

---
+++ src/db/trigger/1_reset_all_schema.sql
@@ -0,0 +1,16 @@
+-- Create the function to reset all sequences
+CREATE OR REPLACE FUNCTION public.reset_all_sequences() RETURNS void AS $$
+DECLARE
+    seq RECORD;
+BEGIN
+    FOR seq IN
+        SELECT sequence_schema, sequence_name
+        FROM information_schema.sequences
+    LOOP
+        EXECUTE 'ALTER SEQUENCE ' || quote_ident(seq.sequence_schema) || '.' || quote_ident(seq.sequence_name) || ' RESTART WITH 1;';
+    END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+
+-- to reset all sequences
+SELECT public.reset_all_sequences();
