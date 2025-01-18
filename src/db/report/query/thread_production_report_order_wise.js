a2f4b60ed7d4dbf00c403ee73959fa302df87a92
Rashik Buksh <rashikbuksh@gmail.com>
2025-01-18 14:36:17 +0600

    Add date range filtering to threadProductionStatusOrderWise query and update parameter definitions

--- src/db/report/query/thread_production_report_order_wise.js
+++ src/db/report/query/thread_production_report_order_wise.js
@@ -6,7 +6,7 @@ import db from '../../index.js';
 export async function threadProductionStatusOrderWise(req, res, next) {
 	if (!(await validateRequest(req, next))) return;
 
-	const { own_uuid } = req?.query;
+	const { own_uuid, from, to } = req?.query;
 
 	// get marketing_uuid from own_uuid
 	let marketingUuid = null;
@@ -37,15 +37,15 @@ export async function threadProductionStatusOrderWise(req, res, next) {
                 ARRAY_AGG(DISTINCT order_entry.swatch_approval_date) as swatch_approval_date,
                 ARRAY_AGG(DISTINCT count_length.count) as count,
                 ARRAY_AGG(DISTINCT count_length.length) as length,
-                SUM(coalesce(order_entry.quantity::float8, 0)::float8) as total_quantity,
-                SUM(coalesce(order_entry_batch_entry_quantity_length.total_weight::float8, 0)::float8) as total_weight,
-                SUM(coalesce(order_entry_batch_entry_quantity_length.yarn_quantity::float8, 0)::float8) as yarn_quantity,
-                SUM(coalesce(order_entry_batch_entry_coning.total_coning_production_quantity::float8, 0)::float8) as total_coning_production_quantity,
-                SUM(coalesce(order_entry.warehouse::float8, 0)::float8) as warehouse,
-                coalesce(thread_challan_sum.total_delivery_delivered_quantity,0)::float8 as total_delivery_delivered_quantity,
-                coalesce(thread_challan_sum.total_delivery_balance_quantity,0)::float8 as total_delivery_balance_quantity,
-                coalesce(thread_challan_sum.total_short_quantity,0)::float8 as total_short_quantity,
-                coalesce(thread_challan_sum.total_reject_quantity,0)::float8 as total_reject_quantity
+                ROUND(SUM(coalesce(order_entry.quantity::float8, 0))::numeric,3)::float8 as total_quantity,
+                ROUND(SUM(coalesce(order_entry_batch_entry_quantity_length.total_weight::float8, 0))::numeric,3)::float8 as total_weight,
+                ROUND(SUM(coalesce(order_entry_batch_entry_quantity_length.yarn_quantity::float8, 0))::numeric,3)::float8 as yarn_quantity,
+                ROUND(SUM(coalesce(order_entry_batch_entry_coning.total_coning_production_quantity::float8, 0))::numeric,3)::float8 as total_coning_production_quantity,
+                ROUND(SUM(coalesce(order_entry.warehouse::float8, 0))::numeric,3)::float8 as warehouse,
+                ROUND(coalesce(thread_challan_sum.total_delivery_delivered_quantity,0)::numeric,3)::float8 as total_delivery_delivered_quantity,
+                ROUND(coalesce(thread_challan_sum.total_delivery_balance_quantity,0)::numeric,3)::float8 as total_delivery_balance_quantity,
+                ROUND(coalesce(thread_challan_sum.total_short_quantity,0)::numeric,3)::float8 as total_short_quantity,
+                ROUND(coalesce(thread_challan_sum.total_reject_quantity,0)::numeric,3)::float8 as total_reject_quantity
             FROM
                 thread.order_entry
             LEFT JOIN
@@ -70,16 +70,17 @@ export async function threadProductionStatusOrderWise(req, res, next) {
             ) order_entry_batch_entry_quantity_length ON order_entry.uuid = order_entry_batch_entry_quantity_length.order_entry_uuid
             LEFT JOIN (
                 SELECT 
-                    SUM(coning_production_quantity) as total_coning_production_quantity,
-                    order_info.uuid as order_entry_uuid
+                    SUM(bep.production_quantity) as total_coning_production_quantity,
+                    order_entry.uuid as order_entry_uuid
                 FROM
-                    thread.batch_entry
+                    thread.batch_entry_production bep
+                LEFT JOIN thread.batch_entry ON bep.batch_entry_uuid = batch_entry.uuid
                 LEFT JOIN 
                     thread.batch ON batch_entry.batch_uuid = batch.uuid
                 LEFT JOIN thread.order_entry ON batch_entry.order_entry_uuid = order_entry.uuid
                 LEFT JOIN thread.order_info ON order_entry.order_info_uuid = order_info.uuid
                 GROUP BY
-                    order_info.uuid
+                    order_entry.uuid
             ) order_entry_batch_entry_coning ON order_entry.uuid = order_entry_batch_entry_coning.order_entry_uuid
              LEFT JOIN (
                 SELECT 
@@ -132,6 +133,7 @@ export async function threadProductionStatusOrderWise(req, res, next) {
             ) thread_batch ON thread_batch.order_info_uuid = order_info.uuid
             WHERE
                 ${own_uuid == null ? sql`TRUE` : sql`order_info.marketing_uuid = ${marketingUuid}`}
+                AND ${from && to ? sql` order_info.created_at::date BETWEEN ${from} AND ${to}` : sql`TRUE`}
             GROUP BY
                 order_info.uuid, party.name, marketing.name, thread_batch.thread_batch, order_info.created_at, order_info.updated_at, order_info.party_uuid, order_info.marketing_uuid, order_info.is_sample, order_info.id, thread_challan_sum.total_delivery_delivered_quantity, thread_challan_sum.total_delivery_balance_quantity, thread_challan_sum.total_short_quantity, thread_challan_sum.total_reject_quantity
             ORDER BY 
--- src/db/report/route.js
+++ src/db/report/route.js
@@ -668,7 +668,11 @@ export const pathReport = {
 			description: 'Thread Production Status Order Wise',
 			tags: ['report'],
 			operationId: 'threadProductionStatusOrderWise',
-			parameters: [],
+			parameters: [
+				SE.parameter_query('from', 'from', '2024-10-01'),
+				SE.parameter_query('to', 'to', '2024-10-31'),
+				SE.parameter_query('own_uuid', 'own_uuid', SE.uuid()),
+			],
 			responses: {
 				200: SE.response_schema(200, {
 					order_entry_uuid: SE.uuid(),
