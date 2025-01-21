8374910f1cb416503685db8f22caf271c22ad2eb
rashikbuksh <rashikbuksh@gmail.com>
2025-01-21 14:36:16 +0600

    Add SQL script for deleting pi_cash_entry and pi_cash records by UUID

---
+++ src/SqlBackup/query-operation/pi-and-pi-cash-entry-delete.sql
@@ -0,0 +1,6 @@
+DELETE FROM commercial.pi_cash_entry WHERE pi_cash_uuid = 'mr45nzE8kNArb2X';
+
+DELETE FROM commercial.pi_cash WHERE uuid = 'mr45nzE8kNArb2X';
+
+
+SELECT * FROM commercial.pi_cash_entry WHERE pi_cash_uuid = 'mr45nzE8kNArb2X';
