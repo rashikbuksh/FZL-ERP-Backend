82cb32b26bd5b96bfdac8baa69e4a0872a2740f3
rashikbuksh <rashikbuksh@gmail.com>
2025-03-25 16:48:20 +0600

    Add item zipper end-wise production query and refactor item name handling

--- src/db/report/query/production_query.js
+++ src/db/report/query/production_query.js
@@ -47,14 +47,127 @@ export async function selectItemWiseProduction(req, res, next) {
                 WHEN (vodf.item_name = 'Nylon' AND vodf.nylon_stopper_name != 'Plastic')
                 THEN vodf.item_name
                 ELSE vodf.item_name 
-            END as item_name
+            END
         UNION 
-        
+        SELECT 
+            'Thread' as item_name,
+            SUM(packing_list_sum.total_packing_list_quantity) as total_production
+        FROM
+            thread.order_info toi
+        LEFT JOIN (
+                SELECT 
+                    oe.order_info_uuid as order_info_uuid,
+                    SUM(ple.quantity) as total_packing_list_quantity
+                FROM 
+                    delivery.packing_list_entry ple
+                LEFT JOIN 
+                    delivery.packing_list pl ON ple.packing_list_uuid = pl.uuid
+                LEFT JOIN
+                    thread.order_entry oe ON ple.thread_order_entry_uuid = oe.uuid
+                WHERE
+                    pl.item_for IN ('thread', 'sample_thread') AND ${from && to ? sql`pl.created_at BETWEEN ${from}::TIMESTAMP AND ${to}::TIMESTAMP + INTERVAL '23 hours 59 minutes 59 seconds'` : sql`1=1`}
+                GROUP BY
+                    oe.order_info_uuid
+        ) packing_list_sum ON toi.uuid = packing_list_sum.order_info_uuid
+        GROUP BY 
+            item_name 
         `;
 
 	try {
 		const data = await db.execute(query);
 		return res.status(200).json({ data });
+	} catch (error) {
+		handleError({ error, res });
+	}
+}
+
+export async function selectItemZipperEndWiseProduction(req, res, next) {
+	if (!(await validateRequest(req, next))) return;
+
+	const { own_uuid } = req.query;
+
+	// get marketing_uuid from own_uuid
+	let marketingUuid = null;
+	const marketingUuidQuery = sql`
+		SELECT uuid
+		FROM public.marketing
+		WHERE user_uuid = ${own_uuid};`;
+
+	try {
+		if (own_uuid) {
+			const marketingUuidData = await db.execute(marketingUuidQuery);
+			marketingUuid = marketingUuidData?.rows[0]?.uuid;
+		}
+
+		const query = sql`
+                    SELECT 
+                        CASE 
+                            WHEN (vodf.item_name = 'Nylon' AND vodf.nylon_stopper_name = 'Plastic')
+                            THEN vodf.item_name || ' ' || 'Plastic'
+                            WHEN (vodf.item_name = 'Nylon' AND vodf.nylon_stopper_name = 'Invisible')
+                            THEN vodf.item_name || ' ' || 'Invisible'
+                            WHEN (vodf.item_name = 'Nylon' AND vodf.nylon_stopper_name != 'Plastic')
+                            THEN vodf.item_name
+                            ELSE vodf.item_name 
+                        END as item_name,
+                        vodf.zipper_number_name,
+                        vodf.end_type_name,
+                        SUM(
+                            packing_list_sum.total_packing_list_quantity
+                        )::float8 as total_production
+                    FROM
+                        zipper.v_order_details_full vodf
+                    LEFT JOIN (
+                        SELECT 
+                            od.uuid as order_description_uuid,
+                            SUM(ple.quantity) as total_packing_list_quantity
+                        FROM 
+                            delivery.packing_list_entry ple
+                        LEFT JOIN 
+                            delivery.packing_list pl ON ple.packing_list_uuid = pl.uuid
+                        LEFT JOIN
+                            zipper.sfg sfg ON ple.sfg_uuid = sfg.uuid
+                        LEFT JOIN
+                            zipper.order_entry oe ON sfg.order_entry_uuid = oe.uuid
+                        LEFT JOIN
+                            zipper.order_description od ON oe.order_description_uuid = od.uuid
+                        WHERE
+                            pl.item_for NOT IN ('thread', 'sample_thread') AND ${from && to ? sql`pl.created_at BETWEEN ${from}::TIMESTAMP AND ${to}::TIMESTAMP + INTERVAL '23 hours 59 minutes 59 seconds'` : sql`1=1`}
+                        GROUP BY
+                            od.uuid
+                    ) packing_list_sum ON vodf.order_description_uuid = packing_list_sum.order_description_uuid
+                    GROUP BY 
+                        CASE 
+                            WHEN (vodf.item_name = 'Nylon' AND vodf.nylon_stopper_name = 'Plastic')
+                            THEN vodf.item_name || ' ' || 'Plastic'
+                            WHEN (vodf.item_name = 'Nylon' AND vodf.nylon_stopper_name = 'Invisible')
+                            THEN vodf.item_name || ' ' || 'Invisible'
+                            WHEN (vodf.item_name = 'Nylon' AND vodf.nylon_stopper_name != 'Plastic')
+                            THEN vodf.item_name
+                            ELSE vodf.item_name 
+                        END,
+                        vodf.zipper_number_name,
+                        vodf.end_type_name
+                    ORDER BY 
+                        item_name, zipper_number_name, end_type_name;
+
+    `;
+		const resultPromise = db.execute(query);
+
+		// group data using item_name
+		const data = await resultPromise;
+
+		if (!Array.isArray(data.rows)) {
+			throw new TypeError('Expected data to be an array');
+		}
+
+		const toast = {
+			status: 200,
+			type: 'select',
+			message: 'Item zipper number end wise approved quantity',
+		};
+
+		return res.status(200).json({ toast, data: data.rows });
 	} catch (error) {
 		handleError({ error, res });
 	}
