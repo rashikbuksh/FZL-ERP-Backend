a4d675b5e6e480273d241e2846c539e41dc10c29
Rashik Buksh <rashikbuksh@gmail.com>
2025-04-06 17:24:52 +0600

    Remove unused status code configuration from API cache middleware

--- src/middleware/api_cache.js
+++ src/middleware/api_cache.js
@@ -2,10 +2,6 @@ import apicache from 'apicache';
 
 const apiCache = apicache.options({
 	enabled: true,
-	statusCodes: {
-		include: [200, 201, 204],
-		exclude: [500, 503],
-	},
 	// appendKey: (req, res) => {
 	// 	return req.originalUrl; // Join keys to form a single cache key
 	// },
