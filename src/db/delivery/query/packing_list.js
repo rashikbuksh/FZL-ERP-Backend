6cb1e324ec1d541796954f7d23e77c8ca787ec4d
Rashik Buksh <rashikbuksh@gmail.com>
2025-04-08 16:58:48 +0600

    Refactor packing list query to correctly handle 'Nylon' item names and ensure 'Thread' is returned for specific item types

--- src/db/delivery/query/packing_list.js
+++ src/db/delivery/query/packing_list.js
@@ -115,9 +115,9 @@ export async function selectAll(req, res, next) {
             WHEN (vod.item_name = 'Nylon' AND vod.nylon_stopper_name = 'Invisible')
             THEN vod.item_name || ' ' || 'Invisible'
             WHEN (vod.item_name = 'Nylon' AND vod.nylon_stopper_name != 'Plastic') 
+			THEN vod.item_name
 			WHEN item_for IN ('thread', 'sample_thread')
 			THEN 'Thread'
-			THEN vod.item_name
             ELSE vod.item_name
         END as item_name,
 		ARRAY_AGG(DISTINCT CASE 
