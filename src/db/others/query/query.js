7740a477db9a64e055f1842a61f19d02d934d3fc
Jobayer Hossen <jobayerhossen64@gmail.com>
2025-04-17 18:32:24 +0600

    refactor: change pi_value calculation to use numeric type for improved precision

--- src/db/others/query/query.js
+++ src/db/others/query/query.js
@@ -1827,7 +1827,7 @@ export async function selectPi(req, res, next) {
 					END
 				ELSE coalesce(pi_cash_entry.pi_cash_quantity,0)  * coalesce(toe.party_price,0) 
 			END
-		)::float8,2) AS pi_value,
+		)::numeric,2)::float8 AS pi_value,
 		ARRAY_AGG(DISTINCT 
 				CASE 
 					WHEN pi_cash_entry.sfg_uuid IS NOT NULL 
