870e96edecef4be39ab72db1c1208e1251601ac7
Rashik Buksh <rashikbuksh@gmail.com>
2025-04-26 14:57:04 +0600

    refactor: move item_for extraction to the beginning of insert function for clarity

--- src/db/delivery/query/packing_list_entry.js
+++ src/db/delivery/query/packing_list_entry.js
@@ -7,6 +7,8 @@ import { packing_list_entry } from '../schema.js';
 export async function insert(req, res, next) {
 	if (!(await validateRequest(req, next))) return;
 
+	const { item_for } = req.body;
+
 	// req.body.map((item) => {
 	// 	if (item.item_for == 'thread' || item.item_for == 'sample_thread') {
 	// 		const { order_entry_uuid } = item;
