58c8f91fc29c72fa02c165c2d620ab3cd5af1b8f
Rashik Buksh <rashikbuksh@gmail.com>
2025-05-20 13:05:48 +0600

    fix: update insert message to reflect the number of inserted entries

--- src/db/lab_dip/query/recipe_entry.js
+++ src/db/lab_dip/query/recipe_entry.js
@@ -19,7 +19,7 @@ export async function insert(req, res, next) {
 		const toast = {
 			status: 201,
 			type: 'insert',
-			message: `${data[0].insertedId} inserted`,
+			message: `${data.length} inserted`,
 		};
 
 		return res.status(201).json({ toast, data });
