85c3e2be3acd51bcfb24b0634c76f5c4ecbae28f
Rashik Buksh <rashikbuksh@gmail.com>
2025-05-22 11:14:16 +0600

    fix: enhance short name aggregation in selectPiEntryByPiUuid function

--- src/db/commercial/query/pi_cash_entry.js
+++ src/db/commercial/query/pi_cash_entry.js
@@ -139,7 +139,9 @@ export async function selectPiEntryByPiUuid(req, res, next) {
 			WITH special_requirements AS (
 				SELECT 
 					order_description_uuid,
-					jsonb_array_elements_text((jsonb_extract_path_text(special_requirement::jsonb, 'values')::jsonb -> 'values')) AS value_uuid
+					jsonb_array_elements_text(
+						(jsonb_extract_path_text(special_requirement::jsonb, 'values')::jsonb -> 'values')
+					) AS value_uuid
 				FROM 
 					zipper.v_order_details_full
 				WHERE
@@ -148,149 +150,173 @@ export async function selectPiEntryByPiUuid(req, res, next) {
 			requirements_with_names AS (
 				SELECT 
 					sr.order_description_uuid,
-					array_agg(p.short_name) AS short_name
+					array_agg(DISTINCT p.short_name) AS short_names
 				FROM 
 					special_requirements sr
 				JOIN 
 					public.properties p ON sr.value_uuid = p.uuid
-                GROUP BY
-                    sr.order_description_uuid
+				GROUP BY
+					sr.order_description_uuid
 			)
-				SELECT
-	                pe.uuid as uuid,
-                    pe.pi_cash_uuid as pi_cash_uuid,
-					pe.pi_cash_quantity::float8 as pi_cash_quantity,
-					(pe.pi_cash_quantity / 12)::float8 as pi_cash_quantity_dzn,
-					pe.created_at as created_at,
-	                pe.updated_at as updated_at,
-					CASE WHEN pe.thread_order_entry_uuid IS NOT NULL THEN true ELSE false END as is_thread_order,
-					sfg.uuid as sfg_uuid,
-					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN vodf.order_info_uuid ELSE toe.order_info_uuid END as order_info_uuid,
-					vodf.order_description_uuid as order_description_uuid,
-					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN vodf.order_number ELSE concat('ST', CASE WHEN toi.is_sample = 1 THEN 'S' ELSE '' END, to_char(toi.created_at, 'YY'), '-', LPAD(toi.id::text, 4, '0')) END as order_number,
-					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN vodf.buyer_name ELSE thread_buyer.name END as buyer_name,
-					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN oe.style ELSE toe.style END as style,
-					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN oe.color ELSE toe.color END as color,
-					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN oe.quantity::float8 ELSE toe.quantity::float8 END as quantity,
-					vodf.item_description as item_description,
-					ARRAY[
-							vodf.nylon_stopper_name,
-							vodf.teeth_color_name,
-							vodf.puller_color_name,
-							vodf.hand_name,
-							vodf.coloring_type_name,
-							vodf.slider_name,
-							vodf.top_stopper_name,
-							vodf.bottom_stopper_name,
-							vodf.logo_type_name,
-							vodf.slider_body_shape_name,
-							vodf.end_user_name,
-							vodf.light_preference_name,
-							vodf.slider_link_name,
-							vodf.teeth_type_name,
-							vodf.description
-						] as short_names,
-					vodf.special_requirement,
-					CONCAT(vodf.item_name, ' Zipper', '-', vodf.zipper_number_short_name, '-', vodf.end_type_short_name, '-', vodf.puller_type_short_name) as pi_item_description,
-					vodf.is_inch,
-					oe.size::float8,
-					CASE
-						WHEN vodf.is_inch = 1
-							THEN 'Inch'
-						WHEN vodf.order_type = 'tape'
-							THEN 'Meter'
-						ELSE 'CM'
-					END as size_unit,
-					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN oe.quantity::float8 ELSE toe.quantity::float8 END as max_quantity,
-					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN oe.company_price::float8 ELSE toe.company_price::float8 END as company_price,
-					CASE 
-						WHEN pe.thread_order_entry_uuid IS NULL 
-						THEN 
-							CASE 
-								WHEN vodf.order_type = 'tape' 
-								THEN oe.company_price::float8 
-								ELSE (oe.company_price/12)::float8 
-							END
-						ELSE toe.company_price::float8 
-					END as company_price_pcs,
-					CASE 
-						WHEN pe.thread_order_entry_uuid IS NULL 
-						THEN 
-							CASE 
-								WHEN vodf.order_type = 'tape' 
-								THEN oe.size::float8 * (oe.company_price::float8)::float8 
-								ELSE (pe.pi_cash_quantity * oe.company_price/12)::float8 
-							END
-						ELSE (pe.pi_cash_quantity * toe.company_price)::float8 
-					END as company_value,
-					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN oe.party_price::float8 ELSE toe.party_price::float8 END as unit_price,
-					CASE 
-						WHEN pe.thread_order_entry_uuid IS NULL 
-						THEN 
-							CASE 
-								WHEN vodf.order_type = 'tape' 
-								THEN oe.party_price::float8 
-								ELSE (oe.party_price/12)::float8 
-							END
-						ELSE toe.party_price::float8 
-					END as unit_price_pcs,
-					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN sfg.pi::float8 ELSE toe.pi::float8 END as given_pi_cash_quantity,
-					CASE 
-						WHEN pe.thread_order_entry_uuid IS NULL 
-						THEN 
-							CASE 
-								WHEN vodf.order_type = 'tape' 
-								THEN oe.size::float8 * (oe.party_price::float8)::float8 
-								ELSE (pe.pi_cash_quantity * oe.party_price/12)::float8 
-							END
-						ELSE (pe.pi_cash_quantity * toe.party_price)::float8 
-					END as value,
-					CASE 
-						WHEN pe.thread_order_entry_uuid IS NULL 
-						THEN 
-							CASE 
-								WHEN vodf.order_type = 'tape'
-								THEN oe.size::float8 * (oe.party_price::float8)::float8
-								ELSE (pe.pi_cash_quantity/12 * oe.party_price)::float8
-							END
-						ELSE (pe.pi_cash_quantity * toe.party_price)::float8 
-					END as value_dzn,
-					CASE 
-						WHEN vodf.order_type = 'tape'
-						THEN 'Meter'
-						ELSE 'Dzn'
-					END as price_unit,
-					CASE 
-						WHEN pe.thread_order_entry_uuid IS NULL 
-						THEN (oe.quantity - sfg.pi)::float8 
-						ELSE (toe.quantity - toe.pi)::float8 
-					END as balance_quantity,
-					pe.thread_order_entry_uuid as thread_order_entry_uuid,
-					toe.count_length_uuid as count_length_uuid,
-					CONCAT(count_length.count,' ', count_length.length) as count_length_name,
-					count_length.count,
-					count_length.length,
-					CASE WHEN pe.uuid IS NOT NULL THEN true ELSE false END as is_checked,
-					vodf.order_type,
-					rwn.short_name as sp_short_names
-	            FROM
-					commercial.pi_cash_entry pe 
-					LEFT JOIN zipper.sfg sfg ON pe.sfg_uuid = sfg.uuid
-	                LEFT JOIN zipper.order_entry oe ON sfg.order_entry_uuid = oe.uuid
-	                LEFT JOIN zipper.v_order_details_full vodf ON oe.order_description_uuid = vodf.order_description_uuid
-					LEFT JOIN thread.order_entry toe ON pe.thread_order_entry_uuid = toe.uuid
-					LEFT JOIN thread.order_info toi ON toe.order_info_uuid = toi.uuid
-					LEFT JOIN thread.count_length count_length ON toe.count_length_uuid = count_length.uuid
-                    LEFT JOIN public.buyer thread_buyer ON toi.buyer_uuid = thread_buyer.uuid
-					LEFT JOIN requirements_with_names rwn ON vodf.order_description_uuid = rwn.order_description_uuid
-				WHERE 
-					pe.pi_cash_uuid = ${req.params.pi_cash_uuid}
-				ORDER BY
-	                vodf.order_number ASC,
-					vodf.item_description ASC,
-					oe.style ASC,
-					oe.color ASC,
-					oe.size ASC 
+			SELECT
+				pe.uuid,
+				pe.pi_cash_uuid,
+				pe.pi_cash_quantity::float8,
+				(pe.pi_cash_quantity / 12)::float8 AS pi_cash_quantity_dzn,
+				pe.created_at,
+				pe.updated_at,
+				(pe.thread_order_entry_uuid IS NOT NULL) AS is_thread_order,
+				sfg.uuid AS sfg_uuid,
+				COALESCE(
+					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN vodf.order_info_uuid ELSE toe.order_info_uuid END,
+					vodf.order_info_uuid
+				) AS order_info_uuid,
+				vodf.order_description_uuid,
+				CASE 
+					WHEN pe.thread_order_entry_uuid IS NULL THEN vodf.order_number
+					ELSE CONCAT('ST', 
+						CASE WHEN toi.is_sample = 1 THEN 'S' ELSE '' END, 
+						TO_CHAR(toi.created_at, 'YY'), '-', LPAD(toi.id::text, 4, '0')
+					)
+				END AS order_number,
+				CASE 
+					WHEN pe.thread_order_entry_uuid IS NULL THEN vodf.buyer_name
+					ELSE thread_buyer.name
+				END AS buyer_name,
+				COALESCE(
+					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN oe.style ELSE toe.style END,
+					oe.style
+				) AS style,
+				COALESCE(
+					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN oe.color ELSE toe.color END,
+					oe.color
+				) AS color,
+				COALESCE(
+					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN oe.quantity::float8 ELSE toe.quantity::float8 END,
+					oe.quantity::float8
+				) AS quantity,
+				vodf.item_description,
+				ARRAY[
+					vodf.nylon_stopper_name,
+					vodf.teeth_color_name,
+					vodf.puller_color_name,
+					vodf.hand_name,
+					vodf.coloring_type_name,
+					vodf.slider_name,
+					vodf.top_stopper_name,
+					vodf.bottom_stopper_name,
+					vodf.logo_type_name,
+					vodf.slider_body_shape_name,
+					vodf.end_user_name,
+					vodf.light_preference_name,
+					vodf.slider_link_name,
+					vodf.teeth_type_name,
+					vodf.description
+				] AS short_names,
+				vodf.special_requirement,
+				CONCAT(
+					vodf.item_name, ' Zipper', '-', 
+					vodf.zipper_number_short_name, '-', 
+					vodf.end_type_short_name, '-', 
+					vodf.puller_type_short_name
+				) AS pi_item_description,
+				vodf.is_inch,
+				oe.size::float8,
+				CASE
+					WHEN vodf.is_inch = 1 THEN 'Inch'
+					WHEN vodf.order_type = 'tape' THEN 'Meter'
+					ELSE 'CM'
+				END AS size_unit,
+				COALESCE(
+					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN oe.quantity::float8 ELSE toe.quantity::float8 END,
+					oe.quantity::float8
+				) AS max_quantity,
+				COALESCE(
+					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN oe.company_price::float8 ELSE toe.company_price::float8 END,
+					oe.company_price::float8
+				) AS company_price,
+				CASE 
+					WHEN pe.thread_order_entry_uuid IS NULL THEN 
+						CASE 
+							WHEN vodf.order_type = 'tape' THEN oe.company_price::float8
+							ELSE (oe.company_price/12)::float8
+						END
+					ELSE toe.company_price::float8
+				END AS company_price_pcs,
+				CASE 
+					WHEN pe.thread_order_entry_uuid IS NULL THEN 
+						CASE 
+							WHEN vodf.order_type = 'tape' THEN oe.size::float8 * oe.company_price::float8
+							ELSE (pe.pi_cash_quantity * oe.company_price/12)::float8
+						END
+					ELSE (pe.pi_cash_quantity * toe.company_price)::float8
+				END AS company_value,
+				COALESCE(
+					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN oe.party_price::float8 ELSE toe.party_price::float8 END,
+					oe.party_price::float8
+				) AS unit_price,
+				CASE 
+					WHEN pe.thread_order_entry_uuid IS NULL THEN 
+						CASE 
+							WHEN vodf.order_type = 'tape' THEN oe.party_price::float8
+							ELSE (oe.party_price/12)::float8
+						END
+					ELSE toe.party_price::float8
+				END AS unit_price_pcs,
+				COALESCE(
+					CASE WHEN pe.thread_order_entry_uuid IS NULL THEN sfg.pi::float8 ELSE toe.pi::float8 END,
+					sfg.pi::float8
+				) AS given_pi_cash_quantity,
+				CASE 
+					WHEN pe.thread_order_entry_uuid IS NULL THEN 
+						CASE 
+							WHEN vodf.order_type = 'tape' THEN oe.size::float8 * oe.party_price::float8
+							ELSE (pe.pi_cash_quantity * oe.party_price/12)::float8
+						END
+					ELSE (pe.pi_cash_quantity * toe.party_price)::float8
+				END AS value,
+				CASE 
+					WHEN pe.thread_order_entry_uuid IS NULL THEN 
+						CASE 
+							WHEN vodf.order_type = 'tape' THEN oe.size::float8 * oe.party_price::float8
+							ELSE (pe.pi_cash_quantity/12 * oe.party_price)::float8
+						END
+					ELSE (pe.pi_cash_quantity * toe.party_price)::float8
+				END AS value_dzn,
+				CASE 
+					WHEN vodf.order_type = 'tape' THEN 'Meter'
+					ELSE 'Dzn'
+				END AS price_unit,
+				CASE 
+					WHEN pe.thread_order_entry_uuid IS NULL THEN (oe.quantity - sfg.pi)::float8
+					ELSE (toe.quantity - toe.pi)::float8
+				END AS balance_quantity,
+				pe.thread_order_entry_uuid,
+				toe.count_length_uuid,
+				CONCAT(count_length.count, ' ', count_length.length) AS count_length_name,
+				count_length.count,
+				count_length.length,
+				(pe.uuid IS NOT NULL) AS is_checked,
+				vodf.order_type,
+				COALESCE(rwn.short_names, ARRAY[]::text[]) AS sp_short_names
+			FROM
+				commercial.pi_cash_entry pe 
+				LEFT JOIN zipper.sfg sfg ON pe.sfg_uuid = sfg.uuid
+				LEFT JOIN zipper.order_entry oe ON sfg.order_entry_uuid = oe.uuid
+				LEFT JOIN zipper.v_order_details_full vodf ON oe.order_description_uuid = vodf.order_description_uuid
+				LEFT JOIN thread.order_entry toe ON pe.thread_order_entry_uuid = toe.uuid
+				LEFT JOIN thread.order_info toi ON toe.order_info_uuid = toi.uuid
+				LEFT JOIN thread.count_length count_length ON toe.count_length_uuid = count_length.uuid
+				LEFT JOIN public.buyer thread_buyer ON toi.buyer_uuid = thread_buyer.uuid
+				LEFT JOIN requirements_with_names rwn ON vodf.order_description_uuid = rwn.order_description_uuid
+			WHERE 
+				pe.pi_cash_uuid = ${req.params.pi_cash_uuid}
+			ORDER BY
+				vodf.order_number ASC,
+				vodf.item_description ASC,
+				oe.style ASC,
+				oe.color ASC,
+				oe.size ASC
 			`;
 
 		const pi_entryPromise = db.execute(query);
