b33d065cd54b3bcc69e5d217c5fb4756aae88063
rashikbuksh <rashikbuksh@gmail.com>
2025-06-23 16:12:45 +0600

    feat: add color_ref_entry_date and color_ref_update_date fields to order entry queries for enhanced tracking

--- src/db/trigger/order_entry_log_after_thread_order_entry.sql
+++ src/db/trigger/order_entry_log_after_thread_order_entry.sql
@@ -3,6 +3,7 @@ RETURNS TRIGGER AS $$
 BEGIN 
     IF (OLD.style IS DISTINCT FROM NEW.style OR
         OLD.color IS DISTINCT FROM NEW.color OR
+        OLD.color_ref IS DISTINCT FROM NEW.color_ref OR
         OLD.quantity IS DISTINCT FROM NEW.quantity OR
         OLD.company_price IS DISTINCT FROM NEW.company_price OR
         OLD.party_price IS DISTINCT FROM NEW.party_price OR
@@ -13,6 +14,7 @@ BEGIN
             thread_order_entry_uuid, 
             style, 
             color,  
+            color_ref,
             quantity, 
             company_price, 
             party_price, 
@@ -23,6 +25,7 @@ BEGIN
             NEW.uuid, 
             NEW.style, 
             NEW.color,  
+            NEW.color_ref,
             NEW.quantity, 
             NEW.company_price, 
             NEW.party_price, 
