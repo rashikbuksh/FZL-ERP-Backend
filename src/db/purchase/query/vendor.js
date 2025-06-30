49be7c6eeb8701a45c001e564990fef41985b8e5
rashikbuksh <rashikbuksh@gmail.com>
2025-06-30 17:47:20 +0600

    fix: add sql import to vendor.js for improved query functionality

--- src/db/purchase/query/vendor.js
+++ src/db/purchase/query/vendor.js
@@ -1,4 +1,4 @@
-import { asc, eq } from 'drizzle-orm';
+import { asc, eq, sql } from 'drizzle-orm';
 import { handleError, validateRequest } from '../../../util/index.js';
 import * as hrSchema from '../../hr/schema.js';
 import db from '../../index.js';
