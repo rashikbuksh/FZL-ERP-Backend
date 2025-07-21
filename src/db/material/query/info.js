9daa9b955c55ce2ca332e1d1627c1aa1d2e22fb7
rashikbuksh <rashikbuksh@gmail.com>
2025-07-21 13:13:46 +0600

    feat: add 'and' import to enhance query capabilities in info.js

--- src/db/material/query/info.js
+++ src/db/material/query/info.js
@@ -1,4 +1,4 @@
-import { asc, eq, lt, sql } from 'drizzle-orm';
+import { and, asc, eq, lt, sql } from 'drizzle-orm';
 import { handleError, validateRequest } from '../../../util/index.js';
 import * as hrSchema from '../../hr/schema.js';
 import db from '../../index.js';
