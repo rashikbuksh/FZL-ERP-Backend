32f1d72d950e397f8a7891712d72ac845e6c9e70
Rashik Buksh <rashikbuksh@gmail.com>
2025-08-23 12:02:44 +0600

    fix: update SQL query in selectOrderEntryFeed for accurate data retrieval and improved readability

--- src/db/dashboard/query/order_entry_feed.js
+++ src/db/dashboard/query/order_entry_feed.js
@@ -8,39 +8,38 @@ export async function selectOrderEntryFeed(req, res, next) {
 	const query = sql`
         SELECT 
             CONCAT('Z', CASE WHEN oi.is_sample = 1 THEN 'S' ELSE '' END, to_char(oi.created_at, 'YY'), '-', oi.id::text) as order_no,
-            pp.name as party_name,
-            pm.name as marketing_name,
+            vodf.party_name,
+            vodf.marketing_name,
             vodf.item_description as item,
             sum(oe.quantity)::float8 as quantity
-
-            FROM
-                zipper.order_info oi
-                LEFT JOIN zipper.order_description od ON oi.uuid = od.order_info_uuid
-                LEFT JOIN zipper.order_entry oe ON od.uuid = oe.order_description_uuid
-                LEFT JOIN zipper.v_order_details_full vodf ON od.uuid = vodf.order_description_uuid
-                LEFT JOIN public.party pp ON oi.party_uuid = pp.uuid
-                LEFT JOIN public.marketing pm ON oi.marketing_uuid = pm.uuid
-            WHERE od.uuid IS NOT NULL
-            GROUP BY
-                order_no, pp.name , vodf.item_description, pm.name
-            
-            UNION 
-            SELECT 
-                CONCAT('ST', CASE WHEN toi.is_sample = 1 THEN 'S' ELSE '' END, to_char(toi.created_at, 'YY'), '-', toi.id::text) as order_no,
-                pp.name as party_name,
-                pm.name as marketing_name,
-                'Sewing Thread' as item,
-                sum(toe.carton_quantity)::float8 as quantity
-            FROM
-                thread.order_info toi
-                LEFT JOIN thread.order_entry toe ON toi.uuid = toe.order_info_uuid
-                LEFT JOIN public.party pp ON toi.party_uuid = pp.uuid
-                LEFT JOIN public.marketing pm ON toi.marketing_uuid = pm.uuid
-            GROUP BY
-                order_no, pp.name, pm.name
-            ORDER BY
-                order_no DESC
-            LIMIT 10;
+        FROM
+            zipper.order_info oi
+            LEFT JOIN zipper.order_description od ON oi.uuid = od.order_info_uuid
+            LEFT JOIN zipper.order_entry oe ON od.uuid = oe.order_description_uuid
+            LEFT JOIN zipper.v_order_details_full vodf ON od.uuid = vodf.order_description_uuid
+        WHERE 
+            od.uuid IS NOT NULL AND vodf.is_cancelled = false
+        GROUP BY
+            order_no, vodf.party_name, vodf.item_description, vodf.marketing_name
+        UNION 
+        SELECT 
+            CONCAT('ST', CASE WHEN toi.is_sample = 1 THEN 'S' ELSE '' END, to_char(toi.created_at, 'YY'), '-', TO_CHAR(toi.id, 'FM000000')) as order_no,
+            pp.name as party_name,
+            pm.name as marketing_name,
+            'Sewing Thread' as item,
+            sum(toe.carton_quantity)::float8 as quantity
+        FROM
+            thread.order_info toi
+            LEFT JOIN thread.order_entry toe ON toi.uuid = toe.order_info_uuid
+            LEFT JOIN public.party pp ON toi.party_uuid = pp.uuid
+            LEFT JOIN public.marketing pm ON toi.marketing_uuid = pm.uuid
+        WHERE 
+            toi.is_cancelled = false
+        GROUP BY
+            order_no, pp.name, pm.name
+        ORDER BY
+            order_no DESC
+        LIMIT 10;
         `;
 
 	const resultPromise = db.execute(query);
