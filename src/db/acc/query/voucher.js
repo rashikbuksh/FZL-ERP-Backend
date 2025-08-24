cfa134e25ce722fe29f30362695f37616f14e857
rashikbuksh <rashikbuksh@gmail.com>
2025-08-24 19:50:15 +0600

    fix: update currency_name format to include currency symbol in parentheses

--- src/db/acc/query/voucher.js
+++ src/db/acc/query/voucher.js
@@ -95,7 +95,7 @@ export async function selectAll(req, res, next) {
 			remarks: voucher.remarks,
 			narration: voucher.narration,
 			currency_uuid: voucher.currency_uuid,
-			currency_name: sql`currency.currency || ' ' || currency.currency_name`,
+			currency_name: sql`currency.currency || ' (' || currency.symbol || ')'`,
 			currency_symbol: currency.symbol,
 			conversion_rate: decimalToNumber(voucher.conversion_rate),
 		})
