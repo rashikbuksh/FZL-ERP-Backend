06cd2585939218f1d63605bfac6bbe94d1f784e3
Jobayer Hossen <jobayerhossen64@gmail.com>
2025-09-10 18:52:38 +0600

    feat: remove 'type' field from selectAll and select functions in group queries

--- src/db/acc/query/group.js
+++ src/db/acc/query/group.js
@@ -90,7 +90,6 @@ export async function selectAll(req, res, next) {
 			updated_by_name: updatedByUser.name,
 			updated_at: group.updated_at,
 			remarks: group.remarks,
-			type: group.type,
 		})
 		.from(group)
 		.leftJoin(head, eq(group.head_uuid, head.uuid))
@@ -130,7 +129,6 @@ export async function select(req, res, next) {
 			updated_by_name: updatedByUser.name,
 			updated_at: group.updated_at,
 			remarks: group.remarks,
-			type: group.type,
 		})
 		.from(group)
 		.leftJoin(head, eq(group.head_uuid, head.uuid))
