a02ec1ec3956e27bfaf5b92c03838b36612921ca
Jobayer Hossen <jobayerhossen64@gmail.com>
2025-09-22 16:00:35 +0600

    feat: add identifier field to selectLedger function for enhanced ledger data

--- src/db/others/query/accounts.js
+++ src/db/others/query/accounts.js
@@ -95,6 +95,7 @@ export async function selectLedger(req, res, next) {
 			label: sql`CONCAT(${accountSchema.ledger.name}, ' (', (COALESCE(voucher_total.total_debit_amount, 0) - COALESCE(voucher_total.total_credit_amount, 0))::float8, ')', CASE WHEN ledger.is_cash_ledger = true THEN ' - Cash' ELSE ' ' END)`,
 			cost_center_count: sql`COALESCE(cost_center.cost_center_count, 0)::float8`,
 			is_cash_ledger: accountSchema.ledger.is_cash_ledger,
+			identifier: accountSchema.ledger.identifier,
 		})
 		.from(accountSchema.ledger)
 		.leftJoin(
